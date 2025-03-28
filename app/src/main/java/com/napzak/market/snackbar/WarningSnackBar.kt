9485646b9648af2ea7942e3a371a7f7cbe410fe8
jangsjw <wkdwodnjs1999@gmail.com>
2025-03-28 18:35:48 +0900

    [ui/#11] WarningSnackBar

---
+++ app/src/main/java/com/napzak/market/snackbar/WarningSnackBar.kt
@@ -0,0 +1,80 @@
+package com.napzak.market.snackbar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.R
+import com.napzak.market.designsystem.theme.NapzakMarketTheme
+
+@Composable
+fun WarningSnackBar(
+    message: String,
+    modifier: Modifier = Modifier,
+) {
+    val colors = NapzakMarketTheme.colors
+    val typography = NapzakMarketTheme.typography
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .background(
+                color = NapzakMarketTheme.colors.white.copy(alpha = 0.5f),
+            )
+            .border(
+                width = 0.92.dp,
+                color = NapzakMarketTheme.colors.red,
+                shape = RoundedCornerShape(50.96.dp),
+            )
+            .padding(
+                horizontal = 15.dp,
+                vertical = 7.dp,
+            )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_warning_snackbar_12),
+                contentDescription = stringResource(id = R.string.warning_snackbar_icon_description),
+                tint = Color.Unspecified,
+                modifier = Modifier.size(12.dp),
+                )
+            Spacer(modifier = Modifier.width(1.5.dp))
+            Text(
+                text = message,
+                style = typography.caption12sb,
+                color = NapzakMarketTheme.colors.red,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WarningSnackBarPreview() {
+    NapzakMarketTheme {
+        WarningSnackBar(
+            message = stringResource(id = R.string.warning_snackbar_genre_limit_message),
+            modifier = Modifier
+        )
+    }
+}
