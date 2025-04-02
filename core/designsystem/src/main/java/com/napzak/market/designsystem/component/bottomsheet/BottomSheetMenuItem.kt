b9cf5006dde4cfc5d64c648b77386d549cce8de2
chrin05 <rinring105@gmail.com>
2025-04-03 01:57:56 +0900

    [ui/#8] dragHandle BottomSheet 구현

---
+++ core/designsystem/src/main/java/com/napzak/market/designsystem/component/bottomsheet/BottomSheetMenuItem.kt
@@ -0,0 +1,89 @@
+package com.napzak.market.designsystem.component.bottomsheet
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.designsystem.R
+import com.napzak.market.designsystem.R.drawable.ic_down_chevron
+import com.napzak.market.designsystem.R.drawable.ic_up_chevron
+import com.napzak.market.designsystem.theme.NapzakMarketTheme
+import com.napzak.market.util.android.noRippleClickable
+
+/**
+ * bottomSheet 안에 들어가는 메뉴 아이템
+ *
+ * @param menuIcon 아이콘 이미지
+ * @param menuName 메뉴 이름
+ * @param onItemClick 아이템 클릭 시 실행됨
+ * @param textStyle 텍스트 스타일
+ * @param textColor 텍스트 컬러
+ * @param isToggleOption 토글 버튼 필요 여부
+ * @param isToggleOpen 토글 버튼의 열림 여부
+ */
+
+@Composable
+fun BottomSheetMenuItem(
+    menuIcon: ImageVector,
+    menuName: String,
+    onItemClick: () -> Unit,
+    textStyle: TextStyle = NapzakMarketTheme.typography.body14b,
+    textColor: Color = NapzakMarketTheme.colors.gray500,
+    isToggleOption: Boolean = false,
+    isToggleOpen: Boolean = false,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .noRippleClickable(onItemClick),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = menuIcon,
+            contentDescription = null,
+            tint = Color.Unspecified,
+        )
+
+        Spacer(Modifier.width(6.dp))
+
+        Text(
+            text = menuName,
+            style = textStyle,
+            color = textColor,
+        )
+
+        Spacer(Modifier.weight(1f))
+
+        if (isToggleOption) {
+            Icon(
+                imageVector = if (isToggleOpen) ImageVector.vectorResource(ic_up_chevron)
+                else ImageVector.vectorResource(ic_down_chevron),
+                contentDescription = null,
+                tint = Color.Unspecified,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BottomSheetMenuItemPreview() {
+    NapzakMarketTheme {
+        BottomSheetMenuItem(
+            menuIcon = ImageVector.vectorResource(R.drawable.ic_delete_24),
+            menuName = "삭제하기",
+            onItemClick = { },
+        )
+    }
+}
---
+++ core/designsystem/src/main/java/com/napzak/market/designsystem/component/bottomsheet/DragHandleBottomSheet.kt
@@ -0,0 +1,138 @@
+package com.napzak.market.designsystem.component.bottomsheet
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.designsystem.theme.NapzakMarketTheme
+import com.napzak.market.designsystem.R
+
+/**
+ * dragHandle 바텀 시트
+ *
+ * @param sheetState BottomSheet 상태
+ * @param onDismissRequest 하향 드래그 또는 BottomSheet 외의 영역 클릭 시 실행됨
+ * @param modifier 수정자 (bottomSheet 높이 지정 가능)
+ * @param bottomSheetContent content 내용 (composable로 작성)
+ */
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DragHandleBottomSheet(
+    sheetState: SheetState,
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+    bottomSheetContent: @Composable ColumnScope.() -> Unit,
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        sheetState = sheetState,
+        shape = RoundedCornerShape(topStart = 31.dp, topEnd = 31.dp),
+        containerColor = NapzakMarketTheme.colors.white,
+        scrimColor = NapzakMarketTheme.colors.transBlack,
+        dragHandle = null,
+    ) {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(top = 17.dp)
+                .padding(horizontal = 20.dp),
+            contentAlignment = Alignment.TopCenter,
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(42.dp)
+                    .height(2.dp)
+                    .background(
+                        color = NapzakMarketTheme.colors.gray100,
+                        shape = RoundedCornerShape(31)
+                    ),
+            )
+
+            Column(
+                modifier = Modifier.padding(top = 44.dp)
+            ) {
+                bottomSheetContent()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun ProductReportBottomSheetPreview() {
+    val sheetState = rememberModalBottomSheetState()
+
+    NapzakMarketTheme {
+        DragHandleBottomSheet(
+            sheetState = sheetState,
+            onDismissRequest = { },
+            modifier = Modifier.height(140.dp),
+        ) {
+            BottomSheetMenuItem(
+                menuIcon = ImageVector.vectorResource(R.drawable.ic_error_24),
+                menuName = "마켓 신고하기",
+                onItemClick = { },
+                textColor = NapzakMarketTheme.colors.red,
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun ProductBottomSheetPreview() {
+    val sheetState = rememberModalBottomSheetState()
+
+    NapzakMarketTheme {
+        DragHandleBottomSheet(
+            sheetState = sheetState,
+            onDismissRequest = { },
+            modifier = Modifier.height(380.dp),
+        ) {
+            BottomSheetMenuItem(
+                menuIcon = ImageVector.vectorResource(R.drawable.ic_edit_24),
+                menuName = "상품 수정",
+                onItemClick = { },
+            )
+
+            Spacer(Modifier.height(20.dp))
+
+            BottomSheetMenuItem(
+                menuIcon = ImageVector.vectorResource(R.drawable.ic_setting_24),
+                menuName = "상품 상태 변경",
+                onItemClick = { },
+                isToggleOption = true,
+                isToggleOpen = false,
+            )
+
+            Spacer(Modifier.height(20.dp))
+
+            BottomSheetMenuItem(
+                menuIcon = ImageVector.vectorResource(R.drawable.ic_delete_24),
+                menuName = "삭제하기",
+                onItemClick = { },
+                textColor = NapzakMarketTheme.colors.red,
+            )
+        }
+    }
+}
