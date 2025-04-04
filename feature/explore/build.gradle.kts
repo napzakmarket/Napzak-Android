7339cc2be4b7fa6b15a766183f7b2aa3abac4b6c
chrin05 <rinring105@gmail.com>
2025-04-04 23:48:58 +0900

    [add/#29] :feature:explore 모듈 추가

---
+++ feature/explore/build.gradle.kts
@@ -0,0 +1,20 @@
+import com.napzak.market.buildlogic.dsl.setNameSpace
+
+plugins {
+    id("com.napzak.market.buildlogic.convention.feature")
+    id("com.napzak.market.buildlogic.convention.compose")
+    id("com.napzak.market.buildlogic.primitive.hilt")
+}
+
+android {
+    setNameSpace("feature.explore")
+}
+
+dependencies {
+
+    implementation(projects.core.designsystem)
+    implementation(projects.core.common)
+    implementation(projects.core.util)
+
+    implementation(libs.androidx.navigation.compose)
+}
