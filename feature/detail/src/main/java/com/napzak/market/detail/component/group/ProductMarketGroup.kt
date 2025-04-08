9e416b98c9d8101422ad68dfb5ac1edafc1766d9
boiledegg <tjrwns2163@naver.com>
2025-04-08 23:24:55 +0900

    [ui/#15] 물품 마켓 정보 컴포넌트 구현

---
+++ feature/detail/src/main/java/com/napzak/market/detail/component/group/ProductMarketGroup.kt
@@ -0,0 +1,147 @@
+package com.napzak.market.detail.component.group
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.napzak.market.designsystem.theme.NapzakMarketTheme
+import com.napzak.market.feature.detail.R
+import com.napzak.market.feature.detail.R.string.detail_product_market_buy
+import com.napzak.market.feature.detail.R.string.detail_product_market_count
+import com.napzak.market.feature.detail.R.string.detail_product_market_group_title
+import com.napzak.market.feature.detail.R.string.detail_product_market_sell
+
+@Composable
+internal fun ProductMarketGroup(
+    marketImage: String,
+    marketName: String,
+    sellCount: String,
+    buyCount: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier.padding(horizontal = 20.dp)) {
+        Text(
+            text = stringResource(detail_product_market_group_title),
+            style = NapzakMarketTheme.typography.body14b.copy(
+                color = NapzakMarketTheme.colors.gray500,
+            ),
+        )
+
+        Spacer(Modifier.height(20.dp))
+
+        ProductMarketProfile(
+            marketImage = marketImage,
+            marketName = marketName,
+            sellCount = sellCount,
+            buyCount = buyCount,
+        )
+    }
+}
+
+@Composable
+private fun ProductMarketProfile(
+    marketImage: String,
+    marketName: String,
+    sellCount: String,
+    buyCount: String,
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    val marketCounts = listOf(
+        detail_product_market_sell to sellCount,
+        detail_product_market_buy to buyCount,
+    )
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .semantics(mergeDescendants = true) { contentDescription = marketName }
+            .background(color = NapzakMarketTheme.colors.gray10, shape = RoundedCornerShape(25.dp))
+            .padding(horizontal = 22.dp, vertical = 17.dp),
+    ) {
+        AsyncImage(
+            model = ImageRequest
+                .Builder(context)
+                .data(marketImage)
+                .crossfade(true)
+                .placeholder(R.drawable.ic_profile)
+                .build(),
+            contentDescription = null,
+            modifier = Modifier.size(60.dp),
+        )
+
+        Spacer(Modifier.width(14.dp))
+
+        Column {
+            Text(
+                text = marketName,
+                style = NapzakMarketTheme.typography.body14b.copy(
+                    color = NapzakMarketTheme.colors.purple500,
+                ),
+            )
+            Spacer(Modifier.height(7.dp))
+            Row(horizontalArrangement = Arrangement.spacedBy(14.dp)) {
+                marketCounts.forEach { (type, count) ->
+                    Row(horizontalArrangement = Arrangement.spacedBy(2.dp)) {
+                        Text(
+                            text = stringResource(type),
+                            style = NapzakMarketTheme.typography.caption12m.copy(
+                                color = NapzakMarketTheme.colors.gray500,
+                            ),
+                        )
+                        Text(
+                            text = stringResource(detail_product_market_count, count),
+                            style = NapzakMarketTheme.typography.caption12m.copy(
+                                color = NapzakMarketTheme.colors.gray500,
+                            ),
+                        )
+                    }
+                }
+            }
+        }
+
+        Spacer(Modifier.weight(1f))
+
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_chevron_right),
+            contentDescription = null,
+            tint = NapzakMarketTheme.colors.gray300,
+            modifier = Modifier.padding(start = 10.dp),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductMarketGroupPreview() {
+    NapzakMarketTheme {
+        ProductMarketGroup(
+            marketImage = "",
+            marketName = "납작한 자기",
+            sellCount = "31",
+            buyCount = "15",
+            modifier = Modifier.background(NapzakMarketTheme.colors.white)
+        )
+    }
+}
