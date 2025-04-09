a1a16f2301f356f1def055643e8d79a500fbaf4d
boiledegg <tjrwns2163@naver.com>
2025-04-09 15:42:23 +0900

    [ui/#15] 상세화면 바텀바 구현

---
+++ feature/detail/src/main/java/com/napzak/market/detail/component/bottombar/ProductDetailBottomBar.kt
@@ -0,0 +1,109 @@
+package com.napzak.market.detail.component.bottombar
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.designsystem.component.button.NapzakButton
+import com.napzak.market.designsystem.theme.NapzakMarketTheme
+import com.napzak.market.feature.detail.R.drawable.ic_chevron_right
+import com.napzak.market.feature.detail.R.drawable.ic_heart_filled_large
+import com.napzak.market.feature.detail.R.drawable.ic_heart_unfilled_large
+import com.napzak.market.feature.detail.R.string.detail_product_button_chat
+
+@Composable
+internal fun ProductDetailBottomBar(
+    isLiked: Boolean,
+    onChatButtonClick: () -> Unit,
+    onLikeButtonClick: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp, vertical = 24.dp),
+    ) {
+        LikeButton(
+            isLiked = isLiked,
+            onClick = { onLikeButtonClick(!isLiked) },
+        )
+
+        Spacer(Modifier.width(8.dp))
+
+        ChatButton(
+            onChatButtonClick = onChatButtonClick,
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+private fun LikeButton(
+    isLiked: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val likeIcon = if (isLiked) ic_heart_filled_large else ic_heart_unfilled_large
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .semantics(
+                mergeDescendants = true,
+                properties = { role = Role.Button },
+            )
+            .clip(RoundedCornerShape(14.dp))
+            .border(width = 1.dp, color = NapzakMarketTheme.colors.gray50)
+            .clickable { onClick() }
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(likeIcon),
+            tint = Color.Unspecified,
+            contentDescription = null,
+            modifier = Modifier.padding(14.dp)
+        )
+    }
+}
+
+@Composable
+private fun ChatButton(
+    onChatButtonClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    NapzakButton(
+        text = stringResource(detail_product_button_chat),
+        icon = ImageVector.vectorResource(ic_chevron_right),
+        onClick = onChatButtonClick,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ChatButtonPreview() {
+    NapzakMarketTheme {
+        ProductDetailBottomBar(
+            isLiked = true,
+            onChatButtonClick = {},
+            onLikeButtonClick = {},
+        )
+    }
+}
