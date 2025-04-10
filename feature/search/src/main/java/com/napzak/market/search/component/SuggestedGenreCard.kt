8c4d9d8863265bd32fb1778dd8515b3c4c441b05
chrin05 <rinring105@gmail.com>
2025-04-10 23:10:30 +0900

    [ui/#40] 추천 장르 component 구현

---
+++ feature/search/src/main/java/com/napzak/market/search/component/SuggestedGenreCard.kt
@@ -0,0 +1,70 @@
+package com.napzak.market.search.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.napzak.market.designsystem.theme.NapzakMarketTheme
+
+@Composable
+internal fun SuggestedGenreCard(
+    genreName: String,
+    imgUrl: String,
+) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .width(100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        AsyncImage(
+            model = ImageRequest
+                .Builder(context)
+                .data(imgUrl)
+                .build(),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(50.dp))
+                .background(color = NapzakMarketTheme.colors.gray100),
+        )
+
+        Spacer(Modifier.height(8.dp))
+
+        Text(
+            text = genreName,
+            style = NapzakMarketTheme.typography.caption12r,
+            color = NapzakMarketTheme.colors.gray300,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SuggestedGenreCardPreview() {
+    NapzakMarketTheme {
+        SuggestedGenreCard(
+            genreName = "짱구는 못말려 날아라 수제김밥",
+            imgUrl = "",
+        )
+    }
+}
