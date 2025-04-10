0e267f1c348c9b24610ab04f7d062b24df7ef8e5
chrin05 <rinring105@gmail.com>
2025-04-10 21:40:39 +0900

    [add/#40] 검색화면 module 추가

---
+++ feature/search/build.gradle.kts
@@ -0,0 +1,22 @@
+import com.napzak.market.buildlogic.dsl.implementation
+import com.napzak.market.buildlogic.dsl.setNameSpace
+
+plugins {
+    id("com.napzak.market.buildlogic.convention.feature")
+    id("com.napzak.market.buildlogic.convention.compose")
+    id("com.napzak.market.buildlogic.primitive.hilt")
+}
+
+android {
+   setNameSpace("feature.search")
+}
+
+dependencies {
+
+    implementation(projects.core.designsystem)
+    implementation(projects.core.common)
+    implementation(projects.core.util)
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.coil.compose)
+}
