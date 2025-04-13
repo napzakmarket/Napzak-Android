e0dc50360103b1227da230a21e8927d349e26d50
chrin05 <rinring105@gmail.com>
2025-04-13 15:49:04 +0900

    [chore/#40] 불필요한 import 제거

--- feature/search/src/main/java/com/napzak/market/search/SearchScreen.kt
+++ feature/search/src/main/java/com/napzak/market/search/SearchScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
