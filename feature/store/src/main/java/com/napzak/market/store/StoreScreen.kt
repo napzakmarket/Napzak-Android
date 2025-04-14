3f693c3013b5069488bf999dc66fa7d7c4b31db2
chrin05 <rinring105@gmail.com>
2025-04-14 15:37:58 +0900

    [chore/#42] 더미데이터 모델에 추가

--- feature/store/src/main/java/com/napzak/market/store/StoreScreen.kt
+++ feature/store/src/main/java/com/napzak/market/store/StoreScreen.kt
@@ -65,8 +65,26 @@ import kotlin.collections.chunked
 import kotlin.collections.forEach
 
 @Composable
-internal fun StoreRoute(modifier: Modifier = Modifier) {
-
+internal fun StoreRoute(
+    modifier: Modifier = Modifier
+) {
+    StoreScreen(
+        selectedTab = MarketTab.SELL,
+        storeInfo = StoreInfo.mockStoreInfo,
+        filteredGenres = emptyList(),
+        sortType = SortType.RECENT,
+        productList = Product.mockMixedProduct,
+        onBackButtonClick = {},
+        onProfileEditClick = {},
+        onTabClicked = {},
+        onGenreFilterClick = {},
+        onFilterClick = {},
+        onGenreDetailNavigate = {},
+        onSortOptionClick = {},
+        onProductDetailNavigate = {},
+        onLikeButtonClick = { id, value -> },
+        modifier = modifier,
+    )
 }
 
 @Composable
@@ -75,7 +93,7 @@ private fun StoreScreen(
     selectedTab: MarketTab,
     filteredGenres: List<Genre>,
     sortType: SortType,
-    productList: List<Product> = Product.mockMixedProduct,
+    productList: List<Product>,
     onProfileEditClick: () -> Unit,
     onTabClicked: (MarketTab) -> Unit,
     onGenreFilterClick: () -> Unit,
@@ -425,30 +443,14 @@ private fun StoreInfoSection(
 @Preview
 @Composable
 private fun StoreScreenPreview(modifier: Modifier = Modifier) {
-    val storeInfo = StoreInfo(
-        storeId = 1,
-        storeNickName = "납작핑",
-        storeDescription = "잡덕입니다. 최애는 짱구, 철수, 흰둥이, 긴토키, 히지카타, 고죠 사토루 관련 상품 판매 및 구매 제시 채팅 언제든 환영합니다 :)",
-        storePhoto = "",
-        storeCover = "",
-        isStoreOwner = true,
-        genrePreferences = listOf(
-            Genre(0, "산리오0"),
-            Genre(1, "산리오1"),
-            Genre(2, "산리오2"),
-            Genre(3, "산리오3"),
-            Genre(4, "산리오4"),
-            Genre(5, "산리오5"),
-            Genre(6, "산리오6")
-        )
-    )
 
     NapzakMarketTheme {
         StoreScreen(
             selectedTab = MarketTab.SELL,
-            storeInfo = storeInfo,
+            storeInfo = StoreInfo.mockStoreInfo,
             filteredGenres = emptyList(),
             sortType = SortType.RECENT,
+            productList = Product.mockMixedProduct,
             onBackButtonClick = {},
             onProfileEditClick = {},
             onTabClicked = {},
