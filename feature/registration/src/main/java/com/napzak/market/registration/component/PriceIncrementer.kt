993ae7016f2d8fe21a443d181aa624d49885fdea
Jong <jm991014@gmail.com>
2025-04-14 23:40:56 +0900

    [ui/#10] PriceIncrementer 구현
    버튼을 누르면 일정 금액 추가

---
+++ feature/registration/src/main/java/com/napzak/market/registration/component/PriceIncrementer.kt
@@ -0,0 +1,92 @@
+package com.napzak.market.registration.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.designsystem.theme.NapzakMarketTheme
+import com.napzak.market.feature.registration.R.string.price_button_1000
+import com.napzak.market.feature.registration.R.string.price_button_10000
+import com.napzak.market.feature.registration.R.string.price_button_100000
+import com.napzak.market.feature.registration.R.string.price_button_5000
+import com.napzak.market.util.android.noRippleClickable
+
+@Composable
+internal fun PriceIncrementer(
+    onClickAddOne: () -> Unit,
+    onClickAddFive: () -> Unit,
+    onClickAddTen: () -> Unit,
+    onClickAddHundred: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        PriceButton(
+            priceUnit = stringResource(price_button_1000),
+            onClick = onClickAddOne,
+        )
+        PriceButton(
+            priceUnit = stringResource(price_button_5000),
+            onClick = onClickAddFive,
+        )
+        PriceButton(
+            priceUnit = stringResource(price_button_10000),
+            onClick = onClickAddTen,
+        )
+        PriceButton(
+            priceUnit = stringResource(price_button_100000),
+            onClick = onClickAddHundred,
+        )
+    }
+}
+
+@Composable
+private fun PriceButton(
+    priceUnit: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = priceUnit,
+        style = NapzakMarketTheme.typography.caption12m.copy(
+            color = NapzakMarketTheme.colors.gray100,
+        ),
+        modifier = modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = NapzakMarketTheme.colors.gray10)
+            .border(
+                width = 1.dp,
+                color = NapzakMarketTheme.colors.gray100,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(horizontal = 10.dp, vertical = 4.dp)
+            .noRippleClickable(onClick),
+    )
+}
+
+@Preview
+@Composable
+private fun PriceIncrementerPreview() {
+    NapzakMarketTheme {
+        PriceIncrementer(
+            onClickAddOne = {},
+            onClickAddFive = {},
+            onClickAddTen = {},
+            onClickAddHundred = {},
+        )
+    }
+}
