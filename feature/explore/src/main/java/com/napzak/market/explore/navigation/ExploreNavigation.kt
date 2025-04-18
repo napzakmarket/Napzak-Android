7c36c0c3602aa8ace158339fbeea1471ad031160
chrin05 <rinring105@gmail.com>
2025-04-18 19:59:30 +0900

    [add/#41] navigation 구현

--- feature/explore/src/main/java/com/napzak/market/explore/navigation/ExploreNavigation.kt
+++ feature/explore/src/main/java/com/napzak/market/explore/navigation/ExploreNavigation.kt
@@ -7,7 +7,9 @@ import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
 import com.napzak.market.common.navigation.MainTabRoute
+import com.napzak.market.common.navigation.Route
 import com.napzak.market.explore.ExploreRoute
+import com.napzak.market.explore.genredetail.GenreDetailRoute
 import kotlinx.serialization.Serializable
 
 const val EMPTY_TEXT = ""
@@ -17,7 +19,14 @@ fun NavController.navigateToExplore(
     navOptions: NavOptions? = null,
 ) = navigate(Explore(searchTerm), navOptions)
 
+fun NavController.navigateToGenreDetail(
+    genreId: Long,
+    navOptions: NavOptions? = null,
+) = navigate(GenreDetail(genreId), navOptions)
+
 fun NavGraphBuilder.exploreGraph(
+    navigateToUp: () -> Unit,
+    navigateToHome: () -> Unit,
     navigateToSearch: () -> Unit,
     navigateToGenreDetail: (Long) -> Unit,
     navigateToProductDetail: (Long) -> Unit,
@@ -34,9 +43,23 @@ fun NavGraphBuilder.exploreGraph(
             modifier = modifier,
         )
     }
+
+    composable<GenreDetail> {
+        GenreDetailRoute(
+            onBackButtonClick = navigateToUp,
+            onHomeNavigate = navigateToHome,
+            onProductClick = navigateToProductDetail,
+            modifier = modifier,
+        )
+    }
 }
 
 @Serializable
 data class Explore(
     val searchTerm: String = EMPTY_TEXT,
 ) : MainTabRoute
+
+@Serializable
+data class GenreDetail(
+    val genreId: Long,
+) : Route
