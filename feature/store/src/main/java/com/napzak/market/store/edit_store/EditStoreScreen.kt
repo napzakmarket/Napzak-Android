c2f13e3481a2bc45829f2184d428adb40dfab420
boiledegg <tjrwns2163@naver.com>
2025-04-22 15:00:48 +0900

    [mod/#17] 프로필 사진 테두리 적용 방식 수정

--- feature/store/src/main/java/com/napzak/market/store/edit_store/EditStoreScreen.kt
+++ feature/store/src/main/java/com/napzak/market/store/edit_store/EditStoreScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -37,10 +38,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -288,14 +286,12 @@ private fun EditStorePhotoSection(
             modifier = Modifier
                 .align(Alignment.TopCenter)
                 .padding(top = 80.dp)
-                .drawBehind {
-                    drawCircle(color = colorScheme.purple100, style = Fill)
-                    drawCircle(
-                        color = colorScheme.white,
-                        style = Stroke(width = 5.dp.toPx()),
-                    )
-                }
-                .size(110.dp),
+                .size(110.dp)
+                .border(
+                    width = 5.dp,
+                    color = NapzakMarketTheme.colors.white,
+                    shape = CircleShape,
+                ),
         )
 
         Icon(
