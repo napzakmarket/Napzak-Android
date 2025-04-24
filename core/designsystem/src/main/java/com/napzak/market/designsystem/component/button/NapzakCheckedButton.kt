b5185d61059e46b47e36c6da55bf9579b50c72ff
yeonjeen <jeendev2019@gmail.com>
2025-04-24 23:38:06 +0900

    [del/#33] modifier 삭제

--- core/designsystem/src/main/java/com/napzak/market/designsystem/component/button/NapzakCheckedButton.kt
+++ core/designsystem/src/main/java/com/napzak/market/designsystem/component/button/NapzakCheckedButton.kt
@@ -67,7 +67,6 @@ fun NapzakCheckedButton(
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier,
         ) {
             Icon(
                 imageVector = checkIcon,
@@ -101,7 +100,6 @@ fun NapzakCheckedButton(
         }
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 private fun NapzakCheckItemPreview() {
