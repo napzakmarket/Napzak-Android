f1f050be74467fc5246972861e564479a6a3545a
yeonjeen <jeendev2019@gmail.com>
2025-04-25 00:20:34 +0900

    [del/#32] 불필요한 임포트 제거

--- feature/onboarding/src/main/java/com/napzak/market/onboarding/termsAgreement/TermsAgreementScreen.kt
+++ feature/onboarding/src/main/java/com/napzak/market/onboarding/termsAgreement/TermsAgreementScreen.kt
@@ -1,7 +1,6 @@
 package com.napzak.market.onboarding.termsAgreement
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,12 +24,12 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.napzak.market.designsystem.component.button.NapzakButton
-import com.napzak.market.designsystem.component.button.NapzakCheckedButton
-import com.napzak.market.designsystem.theme.NapzakMarketTheme
 import com.napzak.market.designsystem.R.drawable.ic_arrow_left
 import com.napzak.market.designsystem.R.drawable.ic_arrow_right
 import com.napzak.market.designsystem.R.drawable.ic_first_step_indicator
+import com.napzak.market.designsystem.component.button.NapzakButton
+import com.napzak.market.designsystem.component.button.NapzakCheckedButton
+import com.napzak.market.designsystem.theme.NapzakMarketTheme
 import com.napzak.market.feature.onboarding.R.string.onboarding_all_agreement_title
 import com.napzak.market.feature.onboarding.R.string.onboarding_next
 import com.napzak.market.feature.onboarding.R.string.onboarding_title
