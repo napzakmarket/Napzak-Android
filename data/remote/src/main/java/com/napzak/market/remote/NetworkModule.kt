046f7542be436d2b42f2b5152daee77112c64f65
boiledegg <tjrwns2163@naver.com>
2025-04-24 18:22:48 +0900

    [feat/#64] BASE_URL retrofit 의존성 모듈 추가

--- data/remote/src/main/java/com/napzak/market/remote/NetworkModule.kt
+++ data/remote/src/main/java/com/napzak/market/remote/NetworkModule.kt
@@ -1,11 +1,14 @@
 package com.napzak.market.remote
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.napzak.market.remote.qualifier.DUMMY
+import com.napzak.market.remote.qualifier.JWT
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import data.remote.BuildConfig
+import data.remote.BuildConfig.BASE_URL
 import data.remote.BuildConfig.DUMMY_URL
 import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
@@ -57,18 +60,36 @@ object NetworkModule {
         }
     }
 
+    @JWT
+    @Provides
+    @Singleton
+    fun providerHeaderInterceptor(): Interceptor = HeaderInterceptor()
+
     @Provides
     @Singleton
     fun provideOkHttpClient(
-        loggingInterceptor: Interceptor
+        loggingInterceptor: Interceptor,
+        @JWT headerInterceptor: Interceptor
     ): OkHttpClient = OkHttpClient.Builder()
         .addInterceptor(loggingInterceptor)
+        .addInterceptor(headerInterceptor)
         .build()
 
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        client: OkHttpClient,
+        factory: Converter.Factory
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(factory)
+        .build()
 
     // TODO: 더미 레트로핏, 삭제 예정
     @Provides
-    fun provideRetrofit(
+    @DUMMY
+    fun provideDummyRetrofit(
         client: OkHttpClient,
         factory: Converter.Factory
     ): Retrofit = Retrofit.Builder()
---
+++ data/remote/src/main/java/com/napzak/market/remote/qualifier/NetworkQualifier.kt
@@ -0,0 +1,11 @@
+package com.napzak.market.remote.qualifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class JWT
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DUMMY
