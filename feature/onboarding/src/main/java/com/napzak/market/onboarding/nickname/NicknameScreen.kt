4248c38f45af5093bc6487bb6d4018fd26c11f47
yeonjeen <jeendev2019@gmail.com>
2025-04-29 01:02:16 +0900

    [refactor/#33] collectAsStateWithLifecycle 적용

--- feature/onboarding/src/main/java/com/napzak/market/onboarding/nickname/NicknameScreen.kt
+++ feature/onboarding/src/main/java/com/napzak/market/onboarding/nickname/NicknameScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,6 +26,7 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.napzak.market.designsystem.R.drawable.ic_arrow_left
 import com.napzak.market.designsystem.R.drawable.ic_second_step_indicator
 import com.napzak.market.designsystem.component.button.NapzakButton
@@ -50,7 +50,7 @@ internal fun NicknameRoute(
     val viewModel: NicknameViewModel = hiltViewModel()
 
     NicknameScreen(
-        uiState = viewModel.uiState.collectAsState().value,
+        uiState = viewModel.uiState.collectAsStateWithLifecycle().value,
         onNicknameChanged = viewModel::onNicknameChanged,
         onBackClick = onBackClick,
         onNextClick = onNextClick,
