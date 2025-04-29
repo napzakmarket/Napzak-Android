341b7a0b2fa6b02254659b824343b293787149f1
boiledegg <tjrwns2163@naver.com>
2025-04-29 18:48:46 +0900

    [feat/#76] 오류 메시지용 확장함수 추가

---
+++ core/util/src/main/java/com/napzak/market/util/android/ThrowableExt.kt
@@ -0,0 +1,31 @@
+package com.napzak.market.util.android
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import retrofit2.HttpException
+
+fun Throwable.getHttpExceptionMessage(): String? = when (this) {
+    is HttpException -> {
+        val errorBody = response()?.errorBody()?.string()
+
+        if (!errorBody.isNullOrBlank()) {
+            runCatching {
+                Json.decodeFromString<ErrorMessage>(errorBody).message
+            }.fold(
+                onSuccess = { it },
+                onFailure = { null }
+            )
+        } else null
+    }
+
+    else -> null
+}
+
+@Serializable
+private data class ErrorMessage(
+    @SerialName("status")
+    val status: Int,
+    @SerialName("message")
+    val message: String,
+)
