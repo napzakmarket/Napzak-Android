96aa2122bf7169a7b49a532edda31b1a8e3a168a
boiledegg <tjrwns2163@naver.com>
2025-04-29 18:42:27 +0900

    [add/#76] BaseResponse 추가

---
+++ data/remote/src/main/java/com/napzak/market/remote/model/BaseResponse.kt
@@ -0,0 +1,13 @@
+package com.napzak.market.remote.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BaseResponse<T>(
+    @SerialName("status") val status: Int,
+    @SerialName("message") val message: String,
+    @SerialName("data") val data: T
+) {
+    val isSuccess get() = status == 200
+}
