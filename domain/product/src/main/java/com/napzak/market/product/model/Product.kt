d4882495a751813f153eb76941a30fb14c97f162
boiledegg <tjrwns2163@naver.com>
2025-04-29 18:22:26 +0900

    [add/#74] Product 모델 추가

--- domain/product/src/main/java/com/napzak/market/product/model/Product.kt
+++ domain/product/src/main/java/com/napzak/market/product/model/Product.kt
@@ -1,4 +1,16 @@
 package com.napzak.market.product.model
 
-class Product {
-}
+data class Product(
+    val productId: Long,
+    val genreName: String,
+    val productName: String,
+    val photo: String,
+    val price: Int,
+    val uploadTime: String,
+    val isInterested: Boolean,
+    val tradeType: String,
+    val tradeStatus: String,
+    val isOwnedByCurrentUser: Boolean,
+    val interestCount: Int,
+    val chatCount: Int
+)
