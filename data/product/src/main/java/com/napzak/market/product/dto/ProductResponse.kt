a03ec0eb17149d36b7e4d7050e6718aeedf70acc
boiledegg <tjrwns2163@naver.com>
2025-04-29 22:26:40 +0900

    [add/#74] dto 추가

---
+++ data/product/src/main/java/com/napzak/market/product/dto/ProductResponse.kt
@@ -0,0 +1,21 @@
+package com.napzak.market.product.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ProductResponse(
+    @SerialName("productId") val productId: Long?,
+    @SerialName("genreName") val genreName: String?,
+    @SerialName("productName") val productName: String?,
+    @SerialName("photo") val photo: String?,
+    @SerialName("price") val price: Int?,
+    @SerialName("uploadTime") val uploadTime: String?,
+    @SerialName("isInterested") val isInterested: Boolean?,
+    @SerialName("tradeType") val tradeType: String?,
+    @SerialName("tradeStatus") val tradeStatus: String?,
+    @SerialName("isPriceNegotiable") val isPriceNegotiable: Boolean?,
+    @SerialName("isOwnedByCurrentUser") val isOwnedByCurrentUser: Boolean?,
+    @SerialName("interestCount") val interestCount: Int?,
+    @SerialName("chatCount") val chatCount: Int?,
+)
