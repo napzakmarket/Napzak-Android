82f80c85160bdd4411c73dcbafc4e88215be234e
boiledegg <tjrwns2163@naver.com>
2025-04-29 22:29:32 +0900

    [feat/#74] service 및 di 모듈 구현

---
+++ data/product/src/main/java/com/napzak/market/product/di/ServiceModule.kt
@@ -0,0 +1,19 @@
+package com.napzak.market.product.di
+
+import com.napzak.market.product.service.ProductRecommendationService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+    @Provides
+    @Singleton
+    fun provideProductRecommendationService(retrofit: Retrofit): ProductRecommendationService =
+        retrofit.create()
+}
