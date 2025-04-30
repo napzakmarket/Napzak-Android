208e5eb08d8460a2273b0b75f09a3d05bbbb137d
chrin05 <rinring105@gmail.com>
2025-05-01 03:53:10 +0900

    [add/#94] dto 구현

---
+++ data/genre/src/main/java/com/napzak/market/genre/dto/GenreItemsResponse.kt
@@ -0,0 +1,9 @@
+package com.napzak.market.genre.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GenreItemsResponse(
+    @SerialName("genreList") val genreList: List<GenreResponse>
+)
