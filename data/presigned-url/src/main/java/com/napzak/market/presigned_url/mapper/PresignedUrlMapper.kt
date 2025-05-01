3261721f1ce84945b6a2d47552f973a940fc4a8a
Jong <jm991014@gmail.com>
2025-05-01 22:40:48 +0900

    [mod/#73] Data Type 일치화

--- data/presigned-url/src/main/java/com/napzak/market/presigned_url/mapper/PresignedUrlMapper.kt
+++ data/presigned-url/src/main/java/com/napzak/market/presigned_url/mapper/PresignedUrlMapper.kt
@@ -4,10 +4,6 @@ import com.napzak.market.presigned_url.dto.ProductPresignedUrlResponse
 import com.napzak.market.presigned_url.dto.ProfilePresignedUrlResponse
 import com.napzak.market.presigned_url.model.PresignedUrlMap
 
-fun ProductPresignedUrlResponse.toPresignedUrlMap() = PresignedUrlMap(
-    presignedUrls = LinkedHashMap(presignedUrls),
-)
+fun ProductPresignedUrlResponse.toPresignedUrlMap() = PresignedUrlMap(presignedUrls)
 
-fun ProfilePresignedUrlResponse.toPresignedUrlMap() = PresignedUrlMap(
-    presignedUrls = LinkedHashMap(presignedUrls),
-)
+fun ProfilePresignedUrlResponse.toPresignedUrlMap() = PresignedUrlMap(presignedUrls)
