ebd290fbe15187f7c46c1807d55adb9736c58bf7
Jong <jm991014@gmail.com>
2025-05-01 23:04:04 +0900

    [mod/#70] Service DTO 타입 수정

--- data/registration/src/main/java/com/napzak/market/registration/service/RegistrationService.kt
+++ data/registration/src/main/java/com/napzak/market/registration/service/RegistrationService.kt
@@ -1,8 +1,9 @@
 package com.napzak.market.registration.service
 
-import com.napzak.market.registration.dto.Product
 import com.napzak.market.registration.dto.PurchaseRegistrationRequest
+import com.napzak.market.registration.dto.PurchaseRegistrationResponse
 import com.napzak.market.registration.dto.SaleRegistrationRequest
+import com.napzak.market.registration.dto.SaleRegistrationResponse
 import com.napzak.market.remote.model.BaseResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -14,32 +15,32 @@ interface RegistrationService {
     @POST("products/sell")
     suspend fun postSaleRegistration(
         @Body request: SaleRegistrationRequest,
-    ): BaseResponse<Product>
+    ): BaseResponse<Long>
 
     @GET("products/sell/modify/{productId}")
     suspend fun getSaleRegistration(
         @Path("productId") productId: Long,
-    ): BaseResponse<Product>
+    ): BaseResponse<SaleRegistrationResponse>
 
     @PUT("products/sell/modify/{productId}")
     suspend fun putSaleRegistration(
         @Path("productId") productId: Long,
         @Body request: SaleRegistrationRequest,
-    ): BaseResponse<Product>
+    ): BaseResponse<Long>
 
     @POST("products/buy")
     suspend fun postPurchaseRegistration(
         @Body request: PurchaseRegistrationRequest,
-    ): BaseResponse<Product>
+    ): BaseResponse<Long>
 
     @GET("products/buy/modify/{productId}")
     suspend fun getPurchaseRegistration(
         @Path("productId") productId: Long,
-        @Body request: PurchaseRegistrationRequest,
-    ): BaseResponse<Product>
+    ): BaseResponse<PurchaseRegistrationResponse>
 
     @PUT("products/buy/modify/{productId}")
     suspend fun putPurchaseRegistration(
         @Path("productId") productId: Long,
-    ): BaseResponse<Product>
+        @Body request: PurchaseRegistrationRequest,
+    ): BaseResponse<Long>
 }
