caf7d7a49c5d773dd8078a4386c2916b2834f53f
Jong <jm991014@gmail.com>
2025-05-01 21:19:52 +0900

    [mod/#70] Data 모듈 의존성 수정

--- data/registration/build.gradle.kts
+++ data/registration/build.gradle.kts
@@ -12,10 +12,8 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(projects.data.remote)
+    implementation(projects.domain.registration)
+
+    implementation(libs.timber)
 }
