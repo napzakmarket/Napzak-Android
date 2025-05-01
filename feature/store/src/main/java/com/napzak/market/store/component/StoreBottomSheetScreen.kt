93d51885256dba9290d3e4c69fa09790cd305b6d
chrin05 <rinring105@gmail.com>
2025-05-01 21:27:50 +0900

    [add/#89] bottomSheet state,screen 구현

---
+++ feature/store/src/main/java/com/napzak/market/store/component/StoreBottomSheetScreen.kt
@@ -0,0 +1,55 @@
+package com.napzak.market.store.component
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.napzak.market.common.type.BottomSheetType
+import com.napzak.market.common.type.SortType
+import com.napzak.market.designsystem.component.bottomsheet.Genre
+import com.napzak.market.designsystem.component.bottomsheet.GenreSearchBottomSheet
+import com.napzak.market.designsystem.component.bottomsheet.SortBottomSheet
+import com.napzak.market.store.store.state.StoreBottomSheetState
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun StoreBottomSheetScreen(
+    storeBottomSheetState: StoreBottomSheetState,
+    sheetState: SheetState,
+    selectedGenres: List<Genre>,
+    genreItems: List<Genre>,
+    sortType: SortType,
+    onDismissRequest: (BottomSheetType) -> Unit,
+    onSortItemClick: (SortType) -> Unit,
+    onTextChange: (String) -> Unit,
+    onGenreSelectButtonClick: (List<Genre>) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    with(storeBottomSheetState) {
+        if (isGenreSearchingBottomSheetVisible) {
+            GenreSearchBottomSheet(
+                initialSelectedGenreList = selectedGenres,
+                sheetState = sheetState,
+                genreItems = genreItems,
+                onDismissRequest = { onDismissRequest(BottomSheetType.GENRE_SEARCHING) },
+                onTextChange = onTextChange,
+                onButtonClick = { newGenres ->
+                    onGenreSelectButtonClick(newGenres)
+                    onDismissRequest(BottomSheetType.GENRE_SEARCHING)
+                },
+            )
+        }
+
+        if (isSortBottomSheetVisible) {
+            SortBottomSheet(
+                selectedSortType = sortType,
+                sheetState = sheetState,
+                onDismissRequest = { onDismissRequest(BottomSheetType.SORT) },
+                onSortItemClick = { newSortOption ->
+                    onSortItemClick(newSortOption)
+                    onDismissRequest(BottomSheetType.SORT)
+                },
+            )
+        }
+    }
+}
---
+++ feature/store/src/main/java/com/napzak/market/store/store/state/StoreBottomSheetState.kt
@@ -0,0 +1,6 @@
+package com.napzak.market.store.store.state
+
+data class StoreBottomSheetState(
+    val isSortBottomSheetVisible: Boolean = false,
+    val isGenreSearchingBottomSheetVisible: Boolean = false,
+)
