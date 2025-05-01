36853b16e3f3f8dac03b357651c64783c8f1d14c
Jong <jm991014@gmail.com>
2025-05-01 23:47:49 +0900

    [mod/#70] product Model 수정

--- domain/registration/src/main/java/com/napzak/market/registration/model/Product.kt
+++ domain/registration/src/main/java/com/napzak/market/registration/model/Product.kt
@@ -2,21 +2,23 @@ package com.napzak.market.registration.model
 
 interface Product
 
-data class SaleProduct(
+data class SaleRegistrationProduct(
     val imageUrls: List<ProductImage>,
     val genreId: Long,
+    val genreName: String,
     val title: String,
     val description: String,
     val price: Int,
-    val productCondition: String?,
+    val productCondition: String,
     val isDeliveryIncluded: Boolean,
     val standardDeliveryFee: Int,
     val halfDeliveryFee: Int,
 ) : Product
 
-data class PurchaseProduct(
+data class PurchaseRegistrationProduct(
     val imageUrls: List<ProductImage>,
     val genreId: Long,
+    val genreName: String,
     val title: String,
     val description: String,
     val price: Int,
