e121f45bc2c9833ebe03370913d315578aa96a0c
yeonjeen <jeendev2019@gmail.com>
2025-05-02 09:29:30 +0900

    [add/#104] 데이터, 도메인 모듈에 Store 모듈 생성

---
+++ data/store/build.gradle.kts
@@ -0,0 +1,18 @@
+import com.napzak.market.buildlogic.dsl.setNameSpace
+
+plugins {
+    id("com.napzak.market.buildlogic.convention.feature")
+    id("com.napzak.market.buildlogic.primitive.hilt")
+    id("com.napzak.market.buildlogic.primitive.retrofit")
+    id("com.napzak.market.buildlogic.primitive.okhttp")
+}
+
+android {
+    setNameSpace("data.store")
+}
+
+dependencies {
+    implementation(projects.data.remote)
+    implementation(libs.timber)
+    implementation(project(":domain:store"))
+}
