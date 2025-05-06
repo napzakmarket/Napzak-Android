07d2982a43add0900eb6e736a776f0da8e528678
boiledegg <tjrwns2163@naver.com>
2025-05-06 13:49:13 +0900

    [feat/#120] :feature:store 빌드 파일에 의존성 추가

--- feature/store/build.gradle.kts
+++ feature/store/build.gradle.kts
@@ -15,7 +15,10 @@ dependencies {
     implementation(projects.core.designsystem)
     implementation(projects.core.common)
     implementation(projects.core.util)
+
     implementation(projects.domain.store)
+    implementation(projects.domain.genre)
+    implementation(projects.domain.presignedUrl)
 
     implementation(libs.androidx.navigation.compose)
     implementation(libs.coil.compose)
