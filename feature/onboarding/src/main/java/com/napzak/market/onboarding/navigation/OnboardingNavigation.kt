ae49551e362899f73f5b1d1dd0a8523337a09f67
yeonjeen <jeendev2019@gmail.com>
2025-05-08 04:47:28 +0900

    [feat/#135] 온보딩 네비게이션 구현

---
+++ feature/onboarding/src/main/java/com/napzak/market/onboarding/navigation/OnboardingNavigation.kt
@@ -0,0 +1,50 @@
+package com.napzak.market.onboarding.navigation
+
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.napzak.market.common.navigation.Route
+import com.napzak.market.onboarding.nickname.NicknameRoute
+import com.napzak.market.onboarding.termsAgreement.TermsAgreementRoute
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToTerms(navOptions: NavOptions? = null) =
+    navigate(Terms, navOptions)
+
+fun NavController.navigateToNickname(navOptions: NavOptions? = null) =
+    navigate(Nickname, navOptions)
+
+fun NavController.navigateToGenre(navOptions: NavOptions? = null) =
+    navigate(Genre, navOptions)
+
+fun NavGraphBuilder.onboardingGraph(
+    onFinish: () -> Unit,
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    composable<Terms> {
+        TermsAgreementRoute(
+            onBackClick = {  },
+            onNextClick = { navController.navigateToNickname() },
+        )
+    }
+
+    composable<Nickname> {
+        NicknameRoute(
+            onBackClick = { navController.navigateToTerms() },
+            onNextClick = onFinish,
+        )
+    }
+}
+
+@Serializable
+object Terms : Route
+
+@Serializable
+object Nickname : Route
+
+@Serializable
+object Genre : Route
