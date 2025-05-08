bd747ac292d6b1fa152e08b777e675565433e20d
yeonjeen <jeendev2019@gmail.com>
2025-05-09 03:47:16 +0900

    [delete/#136] 불필요한 코드 삭제

--- feature/onboarding/src/main/java/com/napzak/market/onboarding/genre/component/GenreGridList.kt
+++ feature/onboarding/src/main/java/com/napzak/market/onboarding/genre/component/GenreGridList.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -17,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.napzak.market.designsystem.theme.NapzakMarketTheme
 import com.napzak.market.onboarding.genre.model.GenreUiModel
