06fdd7b279613d809de5c3d74d22e970023f6609
Jong <jm991014@gmail.com>
2025-05-09 00:45:12 +0900

    [mod/#69] Debounce 호출 위치 조정(ing)

--- feature/registration/src/main/java/com/napzak/market/registration/genre/GenreSearchViewModel.kt
+++ feature/registration/src/main/java/com/napzak/market/registration/genre/GenreSearchViewModel.kt
@@ -1,11 +1,13 @@
 package com.napzak.market.registration.genre
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.napzak.market.registration.genre.state.GenreContract.GenreSearchUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,16 +17,22 @@ class GenreSearchViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(GenreSearchUiState())
     val uiState = _uiState.asStateFlow()
 
-    fun updateSearchTerm(searchTerm: String) = _uiState.update { currentState ->
-        currentState.copy(searchTerm = searchTerm)
+    fun updateSearchTerm(searchTerm: String) {
+        _uiState.update { currentState ->
+            currentState.copy(searchTerm = searchTerm)
+        }
+        updateGenreSearchResult()
     }
 
-    fun debounce() {
-        // TODO: API 연결
+    private fun updateGenreSearchResult() = viewModelScope.launch {
+        // TODO: 여기서 Debounce 구현
     }
 
     fun searchGenre() {
         // TODO: API 연결
     }
 
+    companion object {
+        private const val DEBOUNCE_DELAY = 500L
+    }
 }
