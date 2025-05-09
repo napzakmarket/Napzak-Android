d366e2bfe4deaae8228ef223df2ae899670c1894
Jong <jm991014@gmail.com>
2025-05-09 20:22:27 +0900

    [feat/#130] 팔아요/구해요 물품 등록 API 연동

--- feature/registration/src/main/java/com/napzak/market/registration/purchase/PurchaseRegistrationViewModel.kt
+++ feature/registration/src/main/java/com/napzak/market/registration/purchase/PurchaseRegistrationViewModel.kt
@@ -1,13 +1,19 @@
 package com.napzak.market.registration.purchase
 
 import androidx.lifecycle.viewModelScope
+import com.napzak.market.common.state.UiState
 import com.napzak.market.presigned_url.model.PresignedUrl
 import com.napzak.market.presigned_url.usecase.GetProductPresignedUrlUseCase
 import com.napzak.market.presigned_url.usecase.UploadImageUseCase
 import com.napzak.market.registration.RegistrationContract.RegistrationSideEffect
+import com.napzak.market.registration.RegistrationContract.RegistrationSideEffect.NavigateToDetail
 import com.napzak.market.registration.RegistrationViewModel
 import com.napzak.market.registration.event.GenreEventBus
+import com.napzak.market.registration.model.ProductImage
+import com.napzak.market.registration.model.PurchaseRegistrationProduct
+import com.napzak.market.registration.model.SaleRegistrationProduct
 import com.napzak.market.registration.purchase.state.PurchaseContract.PurchaseUiState
+import com.napzak.market.registration.usecase.RegisterProductUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -20,9 +26,10 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PurchaseRegistrationViewModel @Inject constructor(
-    // TODO: Use Case 구현 필요
+    // TODO: 등록 수정 Use Case
     getProductPresignedUrlUseCase: GetProductPresignedUrlUseCase,
     uploadImageUseCase: UploadImageUseCase,
+    private val registerProductUseCase: RegisterProductUseCase,
 ) : RegistrationViewModel(
     getProductPresignedUrlUseCase,
     uploadImageUseCase,
@@ -51,6 +58,27 @@ class PurchaseRegistrationViewModel @Inject constructor(
     }
 
     override fun uploadProduct(presignedUrls: List<PresignedUrl>) = viewModelScope.launch {
-
+        val purchaseState = _uiState.value
+        val registrationState = registrationUiState.value
+        val product = PurchaseRegistrationProduct(
+            imageUrls = presignedUrls.map {
+                ProductImage(
+                    imageUrl = it.url.substringBefore(VALUE_DELIMITER),
+                    sequence = it.imageName.substringAfter(KEY_DELIMITER).toInt(),
+                )
+            },
+            genreId = registrationState.genre?.genreId ?: 0L,
+            genreName = registrationState.genre?.genreName.orEmpty(),
+            title = registrationState.title,
+            description = registrationState.description,
+            price = registrationState.price.toInt(),
+            isPriceNegotiable = purchaseState.isNegotiable,
+        )
+        registerProductUseCase(product).onSuccess { productId ->
+            updateLoadState(UiState.Success(Unit))
+            _sideEffect.emit(NavigateToDetail(productId))
+        }.onFailure {
+            updateLoadState(UiState.Failure(UPLOADING_PRODUCT_ERROR_MESSAGE))
+        }
     }
 }
--- feature/registration/src/main/java/com/napzak/market/registration/sale/SaleRegistrationViewModel.kt
+++ feature/registration/src/main/java/com/napzak/market/registration/sale/SaleRegistrationViewModel.kt
@@ -1,14 +1,19 @@
 package com.napzak.market.registration.sale
 
 import androidx.lifecycle.viewModelScope
+import com.napzak.market.common.state.UiState
 import com.napzak.market.common.type.ProductConditionType
 import com.napzak.market.presigned_url.model.PresignedUrl
 import com.napzak.market.presigned_url.usecase.GetProductPresignedUrlUseCase
 import com.napzak.market.presigned_url.usecase.UploadImageUseCase
 import com.napzak.market.registration.RegistrationContract.RegistrationSideEffect
+import com.napzak.market.registration.RegistrationContract.RegistrationSideEffect.NavigateToDetail
 import com.napzak.market.registration.RegistrationViewModel
 import com.napzak.market.registration.event.GenreEventBus
+import com.napzak.market.registration.model.ProductImage
+import com.napzak.market.registration.model.SaleRegistrationProduct
 import com.napzak.market.registration.sale.state.SaleContract.SaleUiState
+import com.napzak.market.registration.usecase.RegisterProductUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -20,12 +25,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SaleRegistrationViewModel @Inject constructor(
-    // TODO: Use Case 구현 필요
+    // TODO: 등록 수정 Use Case
     getProductPresignedUrlUseCase: GetProductPresignedUrlUseCase,
     uploadImageUseCase: UploadImageUseCase,
+    private val registerProductUseCase: RegisterProductUseCase,
 ) : RegistrationViewModel(
     getProductPresignedUrlUseCase,
-    uploadImageUseCase
+    uploadImageUseCase,
 ) {
     private val _uiState = MutableStateFlow(SaleUiState())
     val saleUiState = _uiState.asStateFlow()
@@ -79,6 +85,30 @@ class SaleRegistrationViewModel @Inject constructor(
     }
 
     override fun uploadProduct(presignedUrls: List<PresignedUrl>) = viewModelScope.launch {
-
+        val saleState = _uiState.value
+        val registrationState = registrationUiState.value
+        val product = SaleRegistrationProduct(
+            imageUrls = presignedUrls.map {
+                ProductImage(
+                    imageUrl = it.url.substringBefore(VALUE_DELIMITER),
+                    sequence = it.imageName.substringAfter(KEY_DELIMITER).toInt(),
+                )
+            },
+            genreId = registrationState.genre?.genreId ?: 0L,
+            genreName = registrationState.genre?.genreName.orEmpty(),
+            title = registrationState.title,
+            description = registrationState.description,
+            price = registrationState.price.toInt(),
+            productCondition = saleState.condition?.label,
+            isDeliveryIncluded = saleState.isShippingFeeIncluded == true,
+            standardDeliveryFee = saleState.normalShippingFee.toInt(),
+            halfDeliveryFee = saleState.halfShippingFee.toInt(),
+        )
+        registerProductUseCase(product).onSuccess { productId ->
+            updateLoadState(UiState.Success(Unit))
+            _sideEffect.emit(NavigateToDetail(productId))
+        }.onFailure {
+            updateLoadState(UiState.Failure(UPLOADING_PRODUCT_ERROR_MESSAGE))
+        }
     }
 }
