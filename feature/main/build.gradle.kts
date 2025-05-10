29e679ce51d0270e7d10900f455c45e9435dd31a
yeonjeen <jeendev2019@gmail.com>
2025-05-10 23:15:16 +0900

    [build/#35] 메인 그래들에 로그인 의존성 추가

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(projects.feature.detail)
     implementation(projects.feature.registration)
     implementation(projects.feature.mypage)
+    implementation(projects.feature.login)
 
     implementation(projects.core.designsystem)
     implementation(projects.core.common)
