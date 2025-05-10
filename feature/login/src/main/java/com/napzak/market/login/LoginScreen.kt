ea602823a4a9c7e9af546163212cc51eba6bc357
yeonjeen <jeendev2019@gmail.com>
2025-05-10 20:17:10 +0900

    [feat/#35] 로띠 이미지 구현

--- feature/login/src/main/java/com/napzak/market/login/LoginScreen.kt
+++ feature/login/src/main/java/com/napzak/market/login/LoginScreen.kt
@@ -1,2 +1,16 @@
 package com.napzak.market.login
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import com.airbnb.lottie.compose.*
+
+@Composable
+private fun LoginScreen(
+    onKakaoLoginClick: () -> Unit = {},
+){
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(com.napzak.market.designsystem.R.raw.lottie_login))
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+}
