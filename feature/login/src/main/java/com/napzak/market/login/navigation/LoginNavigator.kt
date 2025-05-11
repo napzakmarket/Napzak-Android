b0f1f64f07124f19a6fdb95faa2f0615e74a458c
yeonjeen <jeendev2019@gmail.com>
2025-05-11 16:37:48 +0900

    [feat/#137] 로그인 네비게이션 연결 및 분기처리

--- feature/login/src/main/java/com/napzak/market/login/navigation/LoginNavigator.kt
+++ feature/login/src/main/java/com/napzak/market/login/navigation/LoginNavigator.kt
@@ -1,18 +1,25 @@
 package com.napzak.market.login.navigation
 
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
-import com.napzak.market.common.navigation.Route
 import com.napzak.market.login.LoginRoute
+import com.napzak.market.login.model.LoginFlowRoute
+import com.napzak.market.common.navigation.Route
 import kotlinx.serialization.Serializable
 
 fun NavGraphBuilder.loginGraph(
-    onKakaoLoginClick: () -> Unit,
-    navController: NavHostController,
-    ) {
+    onNavigateToTerms: () -> Unit,
+    onNavigateToHome: () -> Unit,
+) {
     composable<Login> {
-        LoginRoute(onKakaoLoginClick = onKakaoLoginClick)
+        LoginRoute(
+            onRouteChanged = { route ->
+                when (route) {
+                    LoginFlowRoute.Terms -> onNavigateToTerms()
+                    LoginFlowRoute.Main -> onNavigateToHome()
+                }
+            }
+        )
     }
 }
 
