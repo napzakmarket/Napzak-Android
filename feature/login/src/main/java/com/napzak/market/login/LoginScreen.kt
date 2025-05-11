2c09cbd8c03c721634a7511ed9c03a01eed8ed2a
yeonjeen <jeendev2019@gmail.com>
2025-05-11 16:36:37 +0900

    [feat/#137] 카카오 소셜로그인 기능구현

--- feature/login/src/main/java/com/napzak/market/login/LoginScreen.kt
+++ feature/login/src/main/java/com/napzak/market/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.napzak.market.login
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,9 +12,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.airbnb.lottie.compose.LottieAnimation
@@ -21,17 +25,32 @@ import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
+import com.kakao.sdk.user.UserApiClient
 import com.napzak.market.designsystem.R.raw.lottie_login
 import com.napzak.market.designsystem.theme.NapzakMarketTheme
 import com.napzak.market.login.component.KakaoLoginButton
+import com.napzak.market.login.model.LoginFlowRoute
 
 @Composable
 fun LoginRoute(
-    onKakaoLoginClick: () -> Unit,
+    onRouteChanged: (LoginFlowRoute) -> Unit,
     viewModel: LoginViewModel = hiltViewModel(),
 ) {
+    val context = LocalContext.current
+    val uiState by viewModel.uiState.collectAsState()
+
+    LaunchedEffect(uiState.route) {
+        uiState.route?.let { onRouteChanged(it) }
+    }
+
     LoginScreen(
-        onKakaoLoginClick = onKakaoLoginClick,
+        onKakaoLoginClick = {
+            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                if (token != null) {
+                    viewModel.loginWithKakao(token.accessToken)
+                }
+            }
+        },
     )
 }
 
