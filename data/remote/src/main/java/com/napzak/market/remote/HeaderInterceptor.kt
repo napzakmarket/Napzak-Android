8054084c85933617275effc1ce7e0bedd7912f4d
yeonjeen <jeendev2019@gmail.com>
2025-05-12 00:11:39 +0900

    [feat/#137] HeaderInterceptor 구현

--- data/remote/src/main/java/com/napzak/market/remote/HeaderInterceptor.kt
+++ data/remote/src/main/java/com/napzak/market/remote/HeaderInterceptor.kt
@@ -12,16 +12,14 @@ class HeaderInterceptor @Inject constructor(
     private val tokenDataStore: TokenDataStore,
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val newRequest = chain.request().newBuilder()
-            .addAccessTokenHeader()
-            .build()
+        val accessToken = runBlocking { tokenDataStore.getAccessToken() }
+
+        val newRequest = chain.request().newBuilder().apply {
+            if (!accessToken.isNullOrBlank()) {
+                addHeader("Authorization", "Bearer $accessToken")
+            }
+        }.build()
+
         return chain.proceed(newRequest)
-    }
-
-    private fun Request.Builder.addAccessTokenHeader(): Request.Builder = runBlocking {
-        val accessToken = tokenDataStore.getAccessToken()
-
-        // FIXME: 로그인 구현 시 accessToken으로 대체
-        addHeader("Authorization", "Bearer $ACCESS_TOKEN")
     }
 }
