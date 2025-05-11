375988ab79ea76c54d26fa639b44d7afdec8e810
yeonjeen <jeendev2019@gmail.com>
2025-05-11 16:01:39 +0900

    [build/#137] 카카오 로그인 의존성 추가

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -14,8 +14,12 @@ dependencies {
     implementation(projects.core.designsystem)
     implementation(projects.core.common)
     implementation(projects.core.util)
+    implementation(projects.domain.store)
+    implementation(projects.feature.onboarding)
+    implementation(projects.feature.home)
 
     implementation(libs.androidx.navigation.compose)
     implementation(libs.coil.compose)
     implementation(libs.lottie.compose)
+    implementation(libs.v2.user)
 }
