2d6d99354b554692a7f6a7c58ce600bca3cc0e0e
Jong <jm991014@gmail.com>
2025-05-11 23:32:14 +0900

    [feat/#154] UseCase 연동 및 debounce 설정

--- feature/registration/src/main/java/com/napzak/market/registration/genre/GenreSearchViewModel.kt
+++ feature/registration/src/main/java/com/napzak/market/registration/genre/GenreSearchViewModel.kt
@@ -2,34 +2,45 @@ package com.napzak.market.registration.genre
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.napzak.market.genre.usecase.GetGenreNamesUseCase
 import com.napzak.market.registration.genre.state.GenreContract.GenreSearchUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@OptIn(FlowPreview::class)
 @HiltViewModel
 class GenreSearchViewModel @Inject constructor(
-    // TODO: API 연결
+    private val getGenreNamesUseCase: GetGenreNamesUseCase,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(GenreSearchUiState())
     val uiState = _uiState.asStateFlow()
 
-    fun updateSearchTerm(searchTerm: String) {
-        _uiState.update { currentState ->
-            currentState.copy(searchTerm = searchTerm)
+    private val _searchTerm = MutableStateFlow("")
+    val searchTerm = _searchTerm.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            searchTerm.debounce(DEBOUNCE_DELAY).collect {
+                updateGenreSearchResult()
+            }
         }
-        updateGenreSearchResult()
     }
+
+    fun updateSearchTerm(searchTerm: String) = _searchTerm.update { searchTerm }
 
     private fun updateGenreSearchResult() = viewModelScope.launch {
-        // TODO: 여기서 Debounce 구현
-    }
+        getGenreNamesUseCase(searchTerm.value).onSuccess { genres ->
+            _uiState.update { it.copy(genres = genres.toPersistentList()) }
+        }.onFailure {
 
-    fun searchGenre() {
-        // TODO: API 연결
+        }
     }
 
     companion object {
