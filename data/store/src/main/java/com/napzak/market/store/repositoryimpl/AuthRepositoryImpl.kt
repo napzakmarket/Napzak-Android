3e62b459bf9b68b07a1f685383b9fbb5bc753400
yeonjeen <jeendev2019@gmail.com>
2025-05-12 10:14:01 +0900

    [refactor/#137] 레포지터리에 토큰 저장 추가

--- data/store/src/main/java/com/napzak/market/store/repositoryimpl/AuthRepositoryImpl.kt
+++ data/store/src/main/java/com/napzak/market/store/repositoryimpl/AuthRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.napzak.market.store.repositoryimpl
 
+import android.util.Log
+import com.napzak.market.local.datastore.TokenDataStore
 import com.napzak.market.store.datasource.AuthDataSource
 import com.napzak.market.store.mapper.toDomain
 import com.napzak.market.store.model.KakaoLogin
@@ -7,11 +9,20 @@ import com.napzak.market.store.repository.AuthRepository
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
-    private val authDataSource: AuthDataSource
+    private val authDataSource: AuthDataSource,
+    private val tokenDataStore: TokenDataStore,
 ) : AuthRepository {
-    override suspend fun loginWithKakao(accessToken: String): Result<KakaoLogin> = runCatching {
-        authDataSource.loginWithKakao(accessToken).toDomain()
-    }
+    override suspend fun loginWithKakao(accessToken: String): Result<KakaoLogin> =
+        runCatching {
+            val response = authDataSource.loginWithKakao(accessToken)
+
+            tokenDataStore.setTokens(
+                accessToken = response.accessToken,
+                refreshToken = response.refreshToken
+            )
+
+            response.toDomain()
+        }
 
     override suspend fun reissueAccessToken(refreshToken: String): Result<String> = runCatching {
         authDataSource.reissueAccessToken("refreshToken=$refreshToken").accessToken
