710609d9e507d37125c6d79bf78bf8f27743160c
Jong <jm991014@gmail.com>
2025-05-12 22:48:31 +0900

    [feat/#131] TextFieldValue 로버로딩 함수 생성

--- core/designsystem/src/main/java/com/napzak/market/designsystem/component/textfield/NapzakDefaultTextField.kt
+++ core/designsystem/src/main/java/com/napzak/market/designsystem/component/textfield/NapzakDefaultTextField.kt
@@ -20,12 +20,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.napzak.market.designsystem.theme.NapzakMarketTheme
-
 
 /**
  * Default text field: 다른 TextField의 기본이 되는 컴포넌트입니다.
@@ -82,6 +82,75 @@ fun NapzakDefaultTextField(
                     contentAlignment = contentAlignment,
                 ) {
                     if (text.isEmpty()) {
+                        Text(
+                            text = hint,
+                            style = hintTextStyle.copy(color = hintTextColor),
+                        )
+                    }
+                    innerTextField()
+                }
+                suffix?.invoke()
+            }
+        }
+    )
+}
+
+/**
+ * Default text field: 다른 TextField의 기본이 되는 컴포넌트입니다.
+ * String 값이 아닌 TextFieldValue를 받는 InputTextField입니다.
+ *
+ * @param text: 입력 값
+ * @param onTextChange: 입력 값 변경
+ * @param hint: 힌트
+ * @param isSingleLined: single / multi line
+ */
+@Composable
+fun NapzakDefaultTextField(
+    text: TextFieldValue,
+    onTextChange: (TextFieldValue) -> Unit,
+    hint: String,
+    textStyle: TextStyle,
+    hintTextStyle: TextStyle,
+    modifier: Modifier = Modifier,
+    textColor: Color = NapzakMarketTheme.colors.gray500,
+    hintTextColor: Color = NapzakMarketTheme.colors.gray200,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    isSingleLined: Boolean = true,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    suffix: @Composable (() -> Unit)? = null,
+    verticalAlignment: Alignment.Vertical = Alignment.CenterVertically,
+    contentAlignment: Alignment = Alignment.CenterStart,
+    textAlign: TextAlign = TextAlign.Start,
+) {
+    BasicTextField(
+        value = text,
+        onValueChange = onTextChange,
+        textStyle = textStyle.copy(
+            color = textColor,
+            textAlign = textAlign,
+        ),
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        singleLine = isSingleLined,
+        visualTransformation = visualTransformation,
+        modifier = modifier,
+        enabled = enabled,
+        readOnly = readOnly,
+        decorationBox = { innerTextField ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = verticalAlignment,
+            ) {
+                Box(
+                    modifier = Modifier
+                        .weight(1f),
+                    contentAlignment = contentAlignment,
+                ) {
+                    if (text.text.isEmpty()) {
                         Text(
                             text = hint,
                             style = hintTextStyle.copy(color = hintTextColor),
