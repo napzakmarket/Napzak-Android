351e41d3b61ffcac92ff2b5492464be5bc1eecaa
yeonjeen <jeendev2019@gmail.com>
2025-05-19 16:49:10 +0900

    [feat/#176] 로그인 네비게이션 함수 구현

--- feature/login/src/main/java/com/napzak/market/login/navigation/LoginNavigator.kt
+++ feature/login/src/main/java/com/napzak/market/login/navigation/LoginNavigator.kt
@@ -1,11 +1,15 @@
 package com.napzak.market.login.navigation
 
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.napzak.market.common.navigation.Route
 import com.napzak.market.login.LoginRoute
 import com.napzak.market.login.model.LoginFlowRoute
-import com.napzak.market.common.navigation.Route
 import kotlinx.serialization.Serializable
+
+fun NavController.navigateToLogin(navOptions: NavOptions? = null) = navigate(Login, navOptions)
 
 fun NavGraphBuilder.loginGraph(
     onNavigateToTerms: () -> Unit,
