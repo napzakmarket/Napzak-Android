47b09c24ce8b13181eb45456090042991783fa92
yeonjeen <jeendev2019@gmail.com>
2025-05-21 16:09:12 +0900

    [fefat/#216] StoreStateManager 인터페이스 구현체 추가

---
+++ data/store/src/main/java/com/napzak/market/store/repositoryimpl/StoreStateManagerImpl.kt
@@ -0,0 +1,14 @@
+package com.napzak.market.store.repositoryimpl
+
+import com.napzak.market.store.repository.StoreStateManager
+import javax.inject.Inject
+
+class StoreStateManagerImpl @Inject constructor() : StoreStateManager {
+    private var isDeleting = false
+
+    override fun setIsDeleting(value: Boolean) {
+        isDeleting = value
+    }
+
+    override fun isDeleting(): Boolean = isDeleting
+}
