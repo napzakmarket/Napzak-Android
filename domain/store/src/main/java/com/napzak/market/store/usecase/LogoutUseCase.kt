07dba2d3e39011d0df2c96d54b742c81740808ad
yeonjeen <jeendev2019@gmail.com>
2025-06-04 17:27:26 +0900

    [feat/#224] 로그아웃 유즈케이스 구현

---
+++ domain/store/src/main/java/com/napzak/market/store/usecase/LogoutUseCase.kt
@@ -0,0 +1,18 @@
+package com.napzak.market.store.usecase
+
+import com.napzak.market.store.repository.StoreRepository
+import com.napzak.market.util.android.StoreStateManager
+import com.napzak.market.util.android.TokenProvider
+import javax.inject.Inject
+
+class LogoutUseCase @Inject constructor(
+    private val storeRepository: StoreRepository,
+    private val tokenProvider: TokenProvider,
+    private val storeStateManager: StoreStateManager,
+) {
+    suspend operator fun invoke(): Result<Unit> = runCatching {
+        storeRepository.logout()
+        storeStateManager.setIsDeleting(false)
+        tokenProvider.clearAccessToken()
+    }
+}
