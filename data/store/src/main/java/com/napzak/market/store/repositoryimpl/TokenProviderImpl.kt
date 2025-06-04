eeee2e95a04a017169e0fe87ff079fa35b8fe0ab
yeonjeen <jeendev2019@gmail.com>
2025-06-04 14:39:58 +0900

    [feat/#224] refreshToken으로 accessToken 재발급 기능 구현

--- data/store/src/main/java/com/napzak/market/store/repositoryimpl/TokenProviderImpl.kt
+++ data/store/src/main/java/com/napzak/market/store/repositoryimpl/TokenProviderImpl.kt
@@ -1,11 +1,13 @@
 package com.napzak.market.store.repositoryimpl
 
 import com.napzak.market.local.datastore.TokenDataStore
-import com.napzak.market.store.repository.TokenProvider
+import com.napzak.market.store.repository.AuthRepository
+import com.napzak.market.util.android.TokenProvider
 import javax.inject.Inject
 
 class TokenProviderImpl @Inject constructor(
-    private val tokenDataStore: TokenDataStore
+    private val tokenDataStore: TokenDataStore,
+    private val authRepository: AuthRepository,
 ) : TokenProvider {
 
     override suspend fun getAccessToken(): String? = tokenDataStore.getAccessToken()
@@ -18,5 +20,14 @@ class TokenProviderImpl @Inject constructor(
 
     override suspend fun clear() {
         tokenDataStore.clearInfo()
+    }
+
+    override suspend fun reissueAccessToken(): String? {
+        val refreshToken = getRefreshToken() ?: return null
+
+        return authRepository.reissueAccessToken(refreshToken)
+            .onSuccess { newAccessToken ->
+                setTokens(newAccessToken, refreshToken)
+            }.getOrNull()
     }
 }
