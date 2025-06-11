3d73b963795585dfd3ca5a5d72bbcc0bffb27c4b
boiledegg <tjrwns2163@naver.com>
2025-06-11 17:58:39 +0900

    [del/#248] 더미 모듈 제거 및 의존성 제거

--- feature/dummy/src/main/java/com/napzak/market/dummy/state/DummyState.kt
+++
@@ -1,8 +0,0 @@
-package com.napzak.market.dummy.state
-
-import com.napzak.market.common.state.UiState
-import com.napzak.market.dummy.model.DummyUser
-
-data class DummyState(
-    val dummyUsersLoadState: UiState<List<DummyUser>> = UiState.Empty,
-)
