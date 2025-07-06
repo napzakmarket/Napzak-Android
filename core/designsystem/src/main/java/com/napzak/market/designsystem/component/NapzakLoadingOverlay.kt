ec488082dce2f4e92df49e668db5084dfb482d1a
boiledegg <tjrwns2163@naver.com>
2025-07-06 17:06:05 +0900

    [ui/#267] 로딩 컴포넌트 구현

---
+++ core/designsystem/src/main/java/com/napzak/market/designsystem/component/NapzakLoadingOverlay.kt
@@ -0,0 +1,48 @@
+package com.napzak.market.designsystem.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.napzak.market.designsystem.R.raw.lottie_spinner
+import com.napzak.market.designsystem.theme.NapzakMarketTheme
+import com.napzak.market.ui_util.ScreenPreview
+
+@Composable
+fun NapzakLoadingOverlay(
+    modifier: Modifier = Modifier,
+) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(lottie_spinner))
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+    )
+
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
+            modifier = modifier.size(69.dp),
+        )
+    }
+}
+
+@ScreenPreview
+@Composable
+private fun NapzakLoadingOverlayPreview() {
+    NapzakMarketTheme {
+        NapzakLoadingOverlay()
+    }
+}
