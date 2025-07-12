35b0977ec4e5ce969145ff61756b3b398213060c
boiledegg <tjrwns2163@naver.com>
2025-07-12 15:02:22 +0900

    [feat/#277] 시스템바 관리 객체 전역화

--- feature/main/src/main/java/com/napzak/market/main/MainActivity.kt
+++ feature/main/src/main/java/com/napzak/market/main/MainActivity.kt
@@ -5,23 +5,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import com.napzak.market.designsystem.theme.NapzakMarketTheme
+import com.napzak.market.ui_util.LocalSystemBarsColor
+import com.napzak.market.ui_util.SystemBarsColorController
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var systemBarsColorController: SystemBarsColorController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NapzakMarketTheme {
-                MainScreen(
-                    restartApplication = ::restartApplication
-                )
+                SystemBarColorHandler()
+                CompositionLocalProvider(LocalSystemBarsColor provides systemBarsColorController) {
+                    MainScreen(
+                        restartApplication = ::restartApplication
+                    )
+                }
             }
         }
     }
 
+    @Composable
+    private fun SystemBarColorHandler() {
+        systemBarsColorController.Apply(activity = this)
+    }
 
     private fun restartApplication() {
         Intent(this, MainActivity::class.java).apply {
