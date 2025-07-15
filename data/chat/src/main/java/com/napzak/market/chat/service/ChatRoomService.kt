8d0cfbf1179ce9fd7e151b080aa52ad9e79f4de7
boiledegg <tjrwns2163@naver.com>
2025-07-15 15:03:21 +0900

    [feat/#260] ChatRoomService에 메세지 목록 조회 API 추가

--- data/chat/src/main/java/com/napzak/market/chat/service/ChatRoomService.kt
+++ data/chat/src/main/java/com/napzak/market/chat/service/ChatRoomService.kt
@@ -4,6 +4,7 @@ import com.napzak.market.chat.dto.ChatRoomCreateRequest
 import com.napzak.market.chat.dto.ChatRoomCreateResponse
 import com.napzak.market.chat.dto.ChatRoomEnterResponse
 import com.napzak.market.chat.dto.ChatRoomInformationResponse
+import com.napzak.market.chat.dto.ChatRoomMessagesResponse
 import com.napzak.market.chat.dto.ChatRoomPatchProductRequest
 import com.napzak.market.chat.dto.ChatRoomPatchProductResponse
 import com.napzak.market.remote.model.BaseResponse
@@ -13,6 +14,7 @@ import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ChatRoomService {
     @GET("products/chat/{roomId}")
@@ -24,6 +26,13 @@ interface ChatRoomService {
     suspend fun createChatRoom(
         @Body request: ChatRoomCreateRequest
     ): BaseResponse<ChatRoomCreateResponse>
+
+    @GET("chat/rooms/{roomId}/messages")
+    suspend fun getChatRoomMessages(
+        @Path("roomId") roomId: Long,
+        @Query("cursor") cursor: String?,
+        @Query("size") size: Int?
+    ): BaseResponse<ChatRoomMessagesResponse>
 
     @PATCH("chat/rooms/{roomId}/enter")
     suspend fun enterChatRoom(
