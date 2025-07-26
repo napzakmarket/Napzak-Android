130b6b13ee781ef150bf89ae6cbd07b7affc23ba
chrin05 <rinring105@gmail.com>
2025-07-26 14:29:29 +0900

    [mod/#274] default값 위치 수정

--- domain/notification/src/main/java/com/napzak/market/notification/model/EntireNotificationSettings.kt
+++ domain/notification/src/main/java/com/napzak/market/notification/model/EntireNotificationSettings.kt
@@ -2,7 +2,6 @@ package com.napzak.market.notification.model
 
 data class EntireNotificationSettings(
     val deviceToken: String,
-    val platform: String = "ANDROID",
     val isEnabled: Boolean,
     val allowMessage: Boolean,
 )
