a4fa7d1a846b589fcdbea712a7ec11ce27994afd
boiledegg <tjrwns2163@naver.com>
2025-07-28 02:19:04 +0900

    [fix/#283] NapzakLoadingOverlay 경로 오류 수정

--- feature/chat/src/main/java/com/napzak/market/chat/chatroom/ChatRoomScreen.kt
+++ feature/chat/src/main/java/com/napzak/market/chat/chatroom/ChatRoomScreen.kt
@@ -58,8 +58,8 @@ import com.napzak.market.chat.model.ReceiveMessage
 import com.napzak.market.chat.model.StoreBrief
 import com.napzak.market.common.state.UiState
 import com.napzak.market.designsystem.R.drawable.img_empty_chat_room
-import com.napzak.market.designsystem.component.NapzakLoadingOverlay
 import com.napzak.market.designsystem.component.image.ZoomableImageScreen
+import com.napzak.market.designsystem.component.loading.NapzakLoadingOverlay
 import com.napzak.market.designsystem.theme.NapzakMarketTheme
 import com.napzak.market.feature.chat.R.drawable.img_user_blocked_popup
 import com.napzak.market.feature.chat.R.string.chat_room_empty_guide_1
