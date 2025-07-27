8038bfc26e121f41fba3810e92dedfa64dab0fb9
Jong <jm991014@gmail.com>
2025-07-27 22:10:31 +0900

    [ui/#290] GenreEmptyView 이미지 추가

--- feature/registration/src/main/java/com/napzak/market/registration/genre/component/GenreSearchEmptyView.kt
+++ feature/registration/src/main/java/com/napzak/market/registration/genre/component/GenreSearchEmptyView.kt
@@ -1,20 +1,26 @@
 package com.napzak.market.registration.genre.component
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.napzak.market.designsystem.R.drawable.img_genre_not_found
 import com.napzak.market.designsystem.theme.NapzakMarketTheme
 import com.napzak.market.feature.registration.R.string.genre_search_error
 import com.napzak.market.feature.registration.R.string.genre_search_request
@@ -27,9 +33,16 @@ fun GenreSearchEmptyView(
 ) {
     Column(
         modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp),
     ) {
-        Spacer(modifier = Modifier.height(40.dp))
+        Spacer(modifier = Modifier.height(60.dp))
+
+        Icon(
+            imageVector = ImageVector.vectorResource(img_genre_not_found),
+            contentDescription = null,
+            tint = Color.Unspecified,
+        )
 
         Text(
             text = stringResource(genre_search_error),
@@ -38,8 +51,6 @@ fun GenreSearchEmptyView(
                 textAlign = TextAlign.Center,
             ),
         )
-
-        Spacer(modifier = Modifier.height(24.dp))
 
         Text(
             text = stringResource(genre_search_request),
