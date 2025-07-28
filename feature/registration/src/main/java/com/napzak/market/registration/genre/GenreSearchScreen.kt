9b431f899513265e0684f0d0cf34888d41820a0f
Jong <jm991014@gmail.com>
2025-07-28 22:45:08 +0900

    [mod/#290] 장르 검색 결과 분기처리 및 터치 영역 수정

--- feature/registration/src/main/java/com/napzak/market/registration/genre/GenreSearchScreen.kt
+++ feature/registration/src/main/java/com/napzak/market/registration/genre/GenreSearchScreen.kt
@@ -2,10 +2,11 @@ package com.napzak.market.registration.genre
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -13,9 +14,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -31,7 +39,6 @@ import com.napzak.market.registration.genre.state.GenreContract.GenreSearchUiSta
 import com.napzak.market.ui_util.nonClickableStickyHeader
 import com.napzak.market.ui_util.openUrl
 import com.napzak.market.ui_util.throttledNoRippleClickable
-import kotlinx.coroutines.CoroutineScope
 
 private const val GENRE_REQUEST_URL = "https://form.typeform.com/to/C0E09Ymd"
 
@@ -64,7 +71,7 @@ fun GenreSearchRoute(
 }
 
 @Composable
-fun GenreSearchScreen(
+private fun GenreSearchScreen(
     onBackClick: () -> Unit,
     uiState: GenreSearchUiState,
     searchTerm: String,
@@ -74,54 +81,83 @@ fun GenreSearchScreen(
 ) {
     val coroutineScope = rememberCoroutineScope()
     val context = LocalContext.current
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+    var headerHeight by remember { mutableStateOf(260.dp) }
+    val density = LocalDensity.current
+    val verticalArrangement =
+        if (uiState.loadState is UiState.Success) Arrangement.spacedBy(10.dp)
+        else Arrangement.Top
 
-    Column(
-        modifier = Modifier
+    LazyColumn(
+        modifier = modifier
             .fillMaxSize()
             .background(NapzakMarketTheme.colors.gray10),
+        verticalArrangement = verticalArrangement,
     ) {
-        LazyColumn(
-            modifier = modifier.background(NapzakMarketTheme.colors.gray10),
-            verticalArrangement = Arrangement.spacedBy(10.dp),
-        ) {
-            nonClickableStickyHeader {
-                GenreSearchHeader(
-                    onBackClick = onBackClick,
-                    searchTerm = searchTerm,
-                    onSearchTermChange = onSearchTermChange,
-                )
-            }
+        nonClickableStickyHeader {
+            GenreSearchHeader(
+                onBackClick = onBackClick,
+                searchTerm = searchTerm,
+                onSearchTermChange = onSearchTermChange,
+                modifier = Modifier
+                    .onGloballyPositioned { coordinates ->
+                        headerHeight = with(density) { coordinates.size.height.toDp() }
+                    },
+            )
+        }
 
-            when (uiState.loadState) {
-                is UiState.Empty, is UiState.Failure -> item {
+        when (uiState.loadState) {
+            is UiState.Empty, is UiState.Failure -> item {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(screenHeight - headerHeight),
+                    contentAlignment = Alignment.TopCenter,
+                ) {
                     GenreSearchEmptyView(
                         onRequestClick = { context.openUrl(GENRE_REQUEST_URL) },
-                    )
-                }
 
-                is UiState.Success -> {
-                    itemsIndexed(
-                        items = uiState.loadState.data,
-                        key = { _, genre -> genre.genreId },
-                    ) { index, genre ->
-                        Row(
-                            modifier = Modifier
-                                .padding(horizontal = 18.dp, vertical = 4.dp),
-                        ) {
-                            GenreSearchItem(
-                                selectedGenreId = uiState.selectedGenreId,
-                                genre = genre,
-                                onGenreSelect = onGenreSelect,
-                                coroutineScope = coroutineScope,
+                        )
+                }
+            }
+
+            is UiState.Success -> {
+                itemsIndexed(
+                    items = uiState.loadState.data,
+                    key = { _, genre -> genre.genreId },
+                ) { index, genre ->
+                    val topPadding = if (index == 0) 4.dp else 0.dp
+                    val bottomPadding =
+                        if (index == uiState.loadState.data.lastIndex) 4.dp else 0.dp
+
+                    Row(
+                        modifier = Modifier
+                            .padding(
+                                top = topPadding,
+                                bottom = bottomPadding,
                             )
-                        }
+                            .throttledNoRippleClickable(
+                                coroutineScope = coroutineScope,
+                                onClick = { onGenreSelect(genre) },
+                            )
+                            .padding(horizontal = 18.dp),
+                    ) {
+                        GenreSearchItem(
+                            selectedGenreId = uiState.selectedGenreId,
+                            genre = genre,
+                        )
                     }
                 }
+            }
 
-                else -> item {
-                    NapzakLoadingSpinnerOverlay(
-                        modifier = Modifier.fillMaxSize()
-                    )
+            else -> item {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(screenHeight - headerHeight),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    NapzakLoadingSpinnerOverlay()
                 }
             }
         }
@@ -132,8 +168,6 @@ fun GenreSearchScreen(
 private fun GenreSearchItem(
     selectedGenreId: Long?,
     genre: Genre,
-    onGenreSelect: (Genre) -> Unit,
-    coroutineScope: CoroutineScope,
     modifier: Modifier = Modifier,
 ) {
     val textStyle =
@@ -149,11 +183,7 @@ private fun GenreSearchItem(
         style = textStyle,
         modifier = modifier
             .fillMaxWidth()
-            .padding(10.dp)
-            .throttledNoRippleClickable(
-                coroutineScope = coroutineScope,
-                onClick = { onGenreSelect(genre) },
-            ),
+            .padding(10.dp),
     )
 }
 
