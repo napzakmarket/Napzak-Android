6b5bdd293bbadd9eb578710b4151671b777210d6
Jong <jm991014@gmail.com>
2025-07-28 21:15:46 +0900

    [mod/#290] CustomStickyHeader 성능개선

--- core/ui_util/src/main/java/com/napzak/market/ui_util/LazyListExt.kt
+++ core/ui_util/src/main/java/com/napzak/market/ui_util/LazyListExt.kt
@@ -14,17 +14,15 @@ import androidx.compose.ui.Modifier
  * stickyHeader 뒤 content의 클릭 방지를 위한 확장 함수
  */
 @OptIn(ExperimentalFoundationApi::class)
-fun LazyListScope.nonClickableStickyHeader(
+inline fun LazyListScope.nonClickableStickyHeader(
     modifier: Modifier = Modifier,
-    content: @Composable () -> Unit,
-) {
-    stickyHeader {
-        Column(
-            modifier = modifier
-                .fillMaxWidth()
-                .noRippleClickable { },
-        ) {
-            content()
-        }
+    crossinline content: @Composable () -> Unit,
+) = stickyHeader {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .noRippleClickable { },
+    ) {
+        content()
     }
 }
