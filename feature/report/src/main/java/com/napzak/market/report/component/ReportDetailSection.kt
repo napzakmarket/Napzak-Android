57a30232fd1f3b190485a469c766c548b22935d0
boiledegg <tjrwns2163@naver.com>
2025-07-29 22:41:20 +0900

    [refactor/#297] 신고 화면에 BringIntoViewRequester 확장함수 적용
    - FocusState 변경을 감지하여 드롭다운 메뉴를 닫도록 수정

--- feature/report/src/main/java/com/napzak/market/report/component/ReportDetailSection.kt
+++ feature/report/src/main/java/com/napzak/market/report/component/ReportDetailSection.kt
@@ -11,8 +11,12 @@ import androidx.compose.foundation.relocation.BringIntoViewRequester
 import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
@@ -29,8 +33,7 @@ import com.napzak.market.feature.report.R.string.report_input_title_detail
 import com.napzak.market.report.state.ReportState
 import com.napzak.market.report.state.rememberReportState
 import com.napzak.market.report.type.ReportType
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.napzak.market.ui_util.bringIntoView
 
 private val MinHeight = 180.dp
 private const val DETAIL_LENGTH_MAX = 200
@@ -44,16 +47,10 @@ internal fun ReportDetailSection(
 ) {
     val coroutineScope = rememberCoroutineScope()
     val bringIntoViewRequester = remember { BringIntoViewRequester() }
-    val focusEvent: (FocusState) -> Unit = remember {
-        {
-            val imeDelay = 300L
-            if (it.isFocused) {
-                onTextFieldFocus()
-                coroutineScope.launch {
-                    delay(imeDelay)
-                    bringIntoViewRequester.bringIntoView()
-                }
-            }
+    var focusState: FocusState? by remember { mutableStateOf(null) }
+    LaunchedEffect(focusState) {
+        if (focusState?.isFocused == true) {
+            onTextFieldFocus()
         }
     }
 
@@ -81,7 +78,10 @@ internal fun ReportDetailSection(
             contentAlignment = Alignment.TopStart,
             paddingValues = PaddingValues(16.dp),
             modifier = Modifier
-                .onFocusEvent(focusEvent)
+                .onFocusEvent {
+                    bringIntoViewRequester.bringIntoView(coroutineScope, it)
+                    focusState = it
+                }
                 .defaultMinSize(minHeight = MinHeight),
         )
 
