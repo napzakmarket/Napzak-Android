88c2177b0653a0743560efd4f8ae97bd7cbfbfd1
boiledegg <tjrwns2163@naver.com>
2025-08-07 19:07:20 +0900

    [mod/#310] 마켓 관련 탑바 수정 및 systembarpadding 적용

--- feature/store/src/main/java/com/napzak/market/store/store/component/StoreTopBar.kt
+++ feature/store/src/main/java/com/napzak/market/store/store/component/StoreTopBar.kt
@@ -1,21 +1,9 @@
 package com.napzak.market.store.store.component
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.unit.dp
-import com.napzak.market.designsystem.theme.NapzakMarketTheme
-import com.napzak.market.feature.store.R.drawable.ic_kebap
-import com.napzak.market.feature.store.R.drawable.ic_left_chevron
-import com.napzak.market.feature.store.R.string.back_button_description
-import com.napzak.market.ui_util.noRippleClickable
-
+import com.napzak.market.designsystem.component.topbar.MenuTopBar
+import com.napzak.market.designsystem.component.topbar.NavigateUpTopBar
 
 @Composable
 internal fun StoreTopBar(
@@ -24,25 +12,17 @@ internal fun StoreTopBar(
     onMenuButtonClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(
-        modifier = modifier.padding(start = 20.dp, top = 62.dp, end = 20.dp, bottom = 22.dp),
-    ) {
-        Icon(
-            imageVector = ImageVector.vectorResource(ic_left_chevron),
-            contentDescription = stringResource(back_button_description),
-            tint = NapzakMarketTheme.colors.gray200,
-            modifier = Modifier.noRippleClickable(onBackButtonClick),
+    if (isOwner) {
+        NavigateUpTopBar(
+            onNavigateUp = onBackButtonClick,
+            modifier = modifier,
         )
-
-        Spacer(Modifier.weight(1f))
-
-        if (!isOwner) {
-            Icon(
-                imageVector = ImageVector.vectorResource(ic_kebap),
-                contentDescription = null,
-                tint = NapzakMarketTheme.colors.gray200,
-                modifier = Modifier.noRippleClickable(onMenuButtonClick),
-            )
-        }
+    } else {
+        MenuTopBar(
+            title = "",
+            onNavigateUp = onBackButtonClick,
+            onMenuClick = onMenuButtonClick,
+            modifier = modifier,
+        )
     }
 }
