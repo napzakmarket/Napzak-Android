2fcb9c31e339c4a0b9cae8802b662ff78cce5e3e
boiledegg <tjrwns2163@naver.com>
2025-08-07 16:36:43 +0900

    [ui/#310] 메뉴를 포함하는 탑바 구현

---
+++ core/designsystem/src/main/java/com/napzak/market/designsystem/component/topbar/MenuTopBar.kt
@@ -0,0 +1,50 @@
+package com.napzak.market.designsystem.component.topbar
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.designsystem.R.drawable.ic_menu
+import com.napzak.market.designsystem.theme.NapzakMarketTheme
+
+@Composable
+fun MenuTopBar(
+    title: String,
+    onNavigateUp: () -> Unit,
+    onMenuClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    titleAlign: TextAlign = TextAlign.Start,
+    isShadowed: Boolean = true,
+    paddingValues: PaddingValues? = null,
+    topBarColor: NapzakTopBarColor = NapzakTopBarDefault.topBarColor(),
+) {
+    val actions = listOf(
+        NapzakTopBarAction(iconRes = ic_menu, onClick = onMenuClick),
+    )
+
+    NapzakBasicTopBar(
+        onNavigateUp = onNavigateUp,
+        title = title,
+        titleAlign = titleAlign,
+        isShadowed = isShadowed,
+        actions = actions,
+        color = topBarColor,
+        paddingValues = paddingValues ?: NapzakTopBarDefault.topBarInnerPadding,
+        modifier = modifier,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MenuTopBarPreview() {
+    NapzakMarketTheme {
+        MenuTopBar(
+            title = "테스트",
+            onNavigateUp = {},
+            onMenuClick = {},
+            paddingValues = PaddingValues(start = 13.dp, end = 13.dp, top = 34.dp, bottom = 18.dp),
+        )
+    }
+}
