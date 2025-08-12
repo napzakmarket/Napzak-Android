9744e208031a2053374abaafbddec9d0d0f298df
Jong <jm991014@gmail.com>
2025-08-12 13:37:54 +0900

    [refactor/#317]SplashScreen 리팩토링
    1. Preview 함수 추가
    2. Image Painter에서 ImageVector로 수정
    3. SystemBarsController 관련 코드 제거

--- feature/splash/src/main/java/com/napzak/market/splash/SplashScreen.kt
+++ feature/splash/src/main/java/com/napzak/market/splash/SplashScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,12 +14,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.napzak.market.designsystem.R.drawable.ic_logo
 import com.napzak.market.designsystem.theme.NapzakMarketTheme
-import com.napzak.market.ui_util.LocalSystemBarsColor
 import kotlinx.coroutines.delay
 
 @Composable
@@ -30,27 +29,7 @@ internal fun SplashRoute(
     modifier: Modifier = Modifier,
     viewModel: SplashViewModel = hiltViewModel(),
 ) {
-    val localSystemBarsColor = LocalSystemBarsColor.current
-    val splashColor = NapzakMarketTheme.colors.purple500
-    val originalColor = NapzakMarketTheme.colors.white
-
     var isSuccess by remember { mutableStateOf(false) }
-
-    DisposableEffect(Unit) {
-        localSystemBarsColor.setSystemBarColor(
-            statusBarColor = splashColor,
-            navigationBarColor = splashColor,
-            isStatusDarkIcon = false,
-            isNavigationDarkIcon = false,
-        )
-
-        onDispose {
-            localSystemBarsColor.setSystemBarColor(
-                statusBarColor = originalColor,
-                navigationBarColor = originalColor
-            )
-        }
-    }
 
     LaunchedEffect(Unit) {
         val result = viewModel.tryAutoLogin()
@@ -66,27 +45,33 @@ internal fun SplashRoute(
 
     SplashScreen(
         modifier = modifier,
-        backgroundColor = splashColor,
     )
 }
 
 @Composable
 private fun SplashScreen(
-    backgroundColor: Color,
     modifier: Modifier = Modifier,
 ) {
     Box(
         modifier = modifier
             .fillMaxSize()
-            .background(backgroundColor),
+            .background(NapzakMarketTheme.colors.purple500),
         contentAlignment = Alignment.Center,
     ) {
         Image(
-            painter = painterResource(ic_logo),
+            imageVector = ImageVector.vectorResource(ic_logo),
             contentDescription = null,
             modifier = Modifier
                 .fillMaxWidth(0.6f)
                 .aspectRatio(1.7f),
         )
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    NapzakMarketTheme {
+        SplashScreen()
     }
 }
