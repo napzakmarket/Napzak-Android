3174ea981e4f710c81973c875b1d7e1dcc06c1cc
Jong <jm991014@gmail.com>
2025-08-19 01:20:00 +0900

    [del/#317] 미사용 의존성 제거

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -35,5 +35,4 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.accompanist.systemuicontroller)
 }
