b3b206b4ad3dbb3b2caf5824440fead680e724ba
Jong <jm991014@gmail.com>
2025-08-23 17:11:17 +0900

    [add/#320] fallback 캐시삭제 함수 추가

--- app/src/main/java/com/napzak/market/NapzakApplication.kt
+++ app/src/main/java/com/napzak/market/NapzakApplication.kt
@@ -10,9 +10,13 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.kakao.sdk.common.KakaoSdk
 import com.napzak.market.notification.repository.FirebaseRepository
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.io.File
 import javax.inject.Inject
+
+private const val COMPRESSED = "compressed"
 
 @HiltAndroidApp
 class NapzakApplication : Application() {
@@ -29,6 +33,7 @@ class NapzakApplication : Application() {
         initTimber()
         setDayMode()
         getPushToken()
+        clearCacheDir()
     }
 
     private fun initTimber() {
@@ -43,6 +48,18 @@ class NapzakApplication : Application() {
         lifecycleOwner.lifecycleScope.launch {
             lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 firebaseRepository.getPushTokenFromFirebase()
+            }
+        }
+    }
+
+    private fun clearCacheDir() {
+        val dir = File(cacheDir, COMPRESSED)
+
+        lifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            runCatching {
+                if (dir.exists()) dir.deleteRecursively()
+            }.onFailure {
+                Timber.e(it, "Failed to clear compressed cache")
             }
         }
     }
