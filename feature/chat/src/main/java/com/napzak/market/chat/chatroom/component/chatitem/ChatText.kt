8c1dab388e6fb1c243ef0883c14664d9044776b8
boiledegg <tjrwns2163@naver.com>
2025-08-30 03:43:59 +0900

    [fix/#350] 채팅 메시지 줄바꿈 기준 변경

--- feature/chat/src/main/java/com/napzak/market/chat/chatroom/component/chatitem/ChatText.kt
+++ feature/chat/src/main/java/com/napzak/market/chat/chatroom/component/chatitem/ChatText.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.LineBreak
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.napzak.market.designsystem.theme.NapzakMarketTheme
@@ -37,7 +38,9 @@ internal fun ChatText(
     ) {
         Text(
             text = text,
-            style = textStyle,
+            style = textStyle.copy(
+                lineBreak = LineBreak.Simple
+            ),
             color = textColor,
             modifier = Modifier
                 .widthIn(max = maxWidth * 0.72f)
