d5ea53d8c3260324b4f078b157d290f3dcbeb041
boiledegg <tjrwns2163@naver.com>
2025-08-30 02:12:22 +0900

    [refactor/#345] lifeobserver 수정

--- domain/chat/src/main/java/com/napzak/market/chat/usecase/SubscribeChatRoomUseCase.kt
+++ domain/chat/src/main/java/com/napzak/market/chat/usecase/SubscribeChatRoomUseCase.kt
@@ -1,12 +1,20 @@
 package com.napzak.market.chat.usecase
 
 import com.napzak.market.chat.controller.ChatController
+import com.napzak.market.chat.repository.ChatRepository
 import javax.inject.Inject
 
-class SubscribeChatRoomUseCase @Inject constructor(
+class SubscribeChatRoomsUseCase @Inject constructor(
     private val chatController: ChatController,
+    private val chatRepository: ChatRepository,
 ) {
-    suspend operator fun invoke(roomId: Long, storeId: Long): Result<Unit> {
-        return chatController.subscribeChatRoom(roomId, storeId)
+    suspend operator fun invoke(storeId: Long): Result<Unit> {
+        return runCatching {
+            chatRepository.getChatRoomIds().onSuccess { roomIds ->
+                roomIds.forEach { roomId ->
+                    chatController.subscribeChatRoom(roomId, storeId)
+                }
+            }
+        }
     }
 }
