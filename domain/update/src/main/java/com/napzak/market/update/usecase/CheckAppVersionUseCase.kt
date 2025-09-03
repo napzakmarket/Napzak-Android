97f4ca279560d6ff0e9a9dd699d842d7048f1784
chrin05 <rinring105@gmail.com>
2025-09-03 23:41:22 +0900

    [mod/#312] usecase 로직 수정

--- domain/update/src/main/java/com/napzak/market/update/usecase/CheckAppVersionUseCase.kt
+++ domain/update/src/main/java/com/napzak/market/update/usecase/CheckAppVersionUseCase.kt
@@ -6,11 +6,15 @@ import javax.inject.Inject
 class CheckAppVersionUseCase @Inject constructor(
     private val appVersionRepository: AppVersionRepository,
 ) {
-    suspend operator fun invoke(): Boolean =
-        appVersionRepository.getFirebaseVersion()
-            ?.takeIf { it != appVersionRepository.getAppVersion() }
-            ?.let {
-                appVersionRepository.setAppVersion(it)
-                true
-            } == true
+    suspend operator fun invoke(): Boolean {
+        var appVersion = appVersionRepository.getAppVersion()
+        val latestVersion = appVersionRepository.getFirebaseVersion()
+
+        if (appVersion == null || appVersion != latestVersion) {
+            appVersionRepository.setAppVersion(latestVersion!!)
+            appVersion = appVersionRepository.getAppVersion()
+        }
+
+        return appVersion != latestVersion
+    }
 }
