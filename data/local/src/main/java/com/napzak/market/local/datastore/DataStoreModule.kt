1830a226fb7d06662388d637e3fec3b4e259cf0e
chrin05 <rinring105@gmail.com>
2025-09-03 17:27:50 +0900

    [add/#312] NapzakDataStore 추가

--- data/local/src/main/java/com/napzak/market/local/datastore/DataStoreModule.kt
+++ data/local/src/main/java/com/napzak/market/local/datastore/DataStoreModule.kt
@@ -12,13 +12,21 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
+    private const val NAPZAK_PREFERENCE_NAME = "napzak_preference"
     private const val TOKEN_PREFERENCE_NAME = "token_preference"
     private const val NOTIFICATION_PREFERENCE_NAME = "notification_preference"
 
+    private val Context.provideNapzakDataStore by preferencesDataStore(NAPZAK_PREFERENCE_NAME)
     private val Context.provideDataStore by preferencesDataStore(TOKEN_PREFERENCE_NAME)
     private val Context.provideNotificationDataStore by preferencesDataStore(
         NOTIFICATION_PREFERENCE_NAME
     )
+
+    @Provides
+    @Singleton
+    fun provideNapzakDataStore(
+        @ApplicationContext context: Context
+    ) = NapzakDataStore(context.provideNapzakDataStore)
 
     @Provides
     @Singleton
---
+++ data/local/src/main/java/com/napzak/market/local/datastore/NapzakDataStore.kt
@@ -0,0 +1,37 @@
+package com.napzak.market.local.datastore
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class NapzakDataStore @Inject constructor(
+    private val preferenceDataStore: DataStore<Preferences>,
+) {
+    suspend fun getAppVersion(): String? = preferenceDataStore.data
+        .map { it[preferencesAppVersionKey] }
+        .firstOrNull()
+
+    suspend fun setAppVersion(version: String) {
+        preferenceDataStore.edit { preferences ->
+            preferences[preferencesAppVersionKey] = version
+        }
+    }
+
+    suspend fun setFirebaseAppVersion(version: String) {
+        preferenceDataStore.edit { preferences ->
+            preferences[preferencesFirebaseAppVersionKey] = version
+        }
+    }
+
+    companion object {
+        private const val APP_VERSION_KEY = "app_version"
+        private const val FIREBASE_APP_VERSION_KEY = "firebase_app_version"
+
+        val preferencesAppVersionKey = stringPreferencesKey(APP_VERSION_KEY)
+        val preferencesFirebaseAppVersionKey = stringPreferencesKey(FIREBASE_APP_VERSION_KEY)
+    }
+}
