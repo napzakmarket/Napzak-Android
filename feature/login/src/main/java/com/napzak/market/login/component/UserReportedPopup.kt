b09dd2a007e6fde3db9d79a7b595ac98d9ccab35
chrin05 <rinring105@gmail.com>
2025-09-03 15:46:06 +0900

    [mod/#348] string 리소스화

--- feature/login/src/main/java/com/napzak/market/login/component/UserReportedPopup.kt
+++ feature/login/src/main/java/com/napzak/market/login/component/UserReportedPopup.kt
@@ -3,11 +3,15 @@ package com.napzak.market.login.component
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.napzak.market.designsystem.R.drawable.ic_square_red_warning
 import com.napzak.market.designsystem.component.popup.NapzakPopup
 import com.napzak.market.designsystem.theme.NapzakMarketTheme
+import com.napzak.market.feature.login.R.string.reported_popup_subtitle
+import com.napzak.market.feature.login.R.string.reported_popup_title
+import com.napzak.market.feature.login.R.string.reported_popup_confirm_button
 
 @Composable
 fun UserReportedPopup(
@@ -15,11 +19,11 @@ fun UserReportedPopup(
     modifier: Modifier = Modifier,
 ) {
     NapzakPopup(
-        title = "접근이 불가합니다.",
-        subTitle = "해당 계정은 정책 위반으로 인해\n앱 서비스 접근이 불가합니다.",
+        title = stringResource(reported_popup_title),
+        subTitle = stringResource(reported_popup_subtitle),
         icon = ImageVector.vectorResource(ic_square_red_warning),
         buttonColor = NapzakMarketTheme.colors.red,
-        buttonText = "확인",
+        buttonText = stringResource(reported_popup_confirm_button),
         buttonIcon = null,
         onButtonClick = onButtonClick,
         modifier = modifier,
