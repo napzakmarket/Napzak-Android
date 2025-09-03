f58083103e566bb225a00d80896048585602121c
chrin05 <rinring105@gmail.com>
2025-09-03 17:28:36 +0900

    [feat/#312] firebase에서 앱 버전 remoteConfig로 가져오기

--- app/src/main/java/com/napzak/market/NapzakApplication.kt
+++ app/src/main/java/com/napzak/market/NapzakApplication.kt
@@ -7,7 +7,11 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.google.firebase.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.google.firebase.remoteconfig.remoteConfig
 import com.kakao.sdk.common.KakaoSdk
+import com.napzak.market.local.datastore.NapzakDataStore
 import com.napzak.market.notification.repository.FirebaseRepository
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.Dispatchers
@@ -17,12 +21,17 @@ import java.io.File
 import javax.inject.Inject
 
 private const val COMPRESSED = "compressed"
+private const val ANDROID_APP_VERSION = "android_app_version"
 
 @HiltAndroidApp
 class NapzakApplication : Application() {
 
     @Inject
     lateinit var firebaseRepository: FirebaseRepository
+
+    @Inject
+    lateinit var napzakDataStore: NapzakDataStore
+
     private val lifecycleOwner: LifecycleOwner
         get() = ProcessLifecycleOwner.get()
 
@@ -34,6 +43,7 @@ class NapzakApplication : Application() {
         setDayMode()
         getPushToken()
         clearCacheDir()
+        getAppVersion()
     }
 
     private fun initTimber() {
@@ -60,6 +70,24 @@ class NapzakApplication : Application() {
                 if (dir.exists()) dir.deleteRecursively()
             }.onFailure {
                 Timber.e(it, "Failed to clear compressed cache")
+            }
+        }
+    }
+
+    private fun getAppVersion() {
+        val remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 0 // TODO : 시간 바꾸기
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(mapOf(ANDROID_APP_VERSION to "0.0.0"))
+
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            if (it.isSuccessful) {
+                lifecycleOwner.lifecycleScope.launch {
+                    val appVersion = remoteConfig.getString(ANDROID_APP_VERSION)
+                    napzakDataStore.setFirebaseAppVersion(appVersion)
+                }
             }
         }
     }
