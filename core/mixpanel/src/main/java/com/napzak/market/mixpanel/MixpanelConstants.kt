948876054fa09b1d149ad5c22e2ab7cb3b865c5c
Jong <jm991014@gmail.com>
2025-09-04 16:42:37 +0900

    [add/#308] MixpanelConstants 추가

---
+++ core/mixpanel/src/main/java/com/napzak/market/mixpanel/MixpanelConstants.kt
@@ -0,0 +1,28 @@
+package com.napzak.market.mixpanel
+
+object MixpanelConstants {
+    // Event Name
+    const val VIEWED_EXPLORE = "Viewed Explore"
+    const val APPLIED_GENRE_FILTER = "Applied Genre Filter"
+    const val APPLIED_ARRAY_FILTER = "Applied array Filter"
+    const val VIEWED_PRODUCT = "Viewed Product"
+    const val STARTED_CHAT = "Started Chat"
+
+    // Property Keys
+    const val TAB = "tab"
+    const val FILTER_COUNT = "filter_count"
+    const val SORT = "sort"
+    const val POST_ID = "post_id"
+    const val POST_TYPE = "post_type"
+    const val USER_ROLE = "user_role"
+
+    // Property Values
+    const val FOR_SALE = "for_sale"
+    const val WANTED = "wanted"
+    const val LATEST = "for_sale"
+    const val POPULAR = "popular"
+    const val HIGH_PRICE = "high_price"
+    const val LOW_PRICE = "low_price"
+    const val BUYER = "buyer"
+    const val SELLER = "seller"
+}
