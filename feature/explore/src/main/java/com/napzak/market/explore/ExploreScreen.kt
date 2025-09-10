0c0b2e328fbf67582c5ef5518b5fb70fccb59b42
chrin05 <rinring105@gmail.com>
2025-09-10 22:46:44 +0900

    [mod/#361] 필터부분 항상 보이도록 수정

--- feature/explore/src/main/java/com/napzak/market/explore/ExploreScreen.kt
+++ feature/explore/src/main/java/com/napzak/market/explore/ExploreScreen.kt
@@ -219,8 +219,6 @@ private fun ExploreScreen(
             }
         }
     }
-
-
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -281,48 +279,49 @@ private fun ExploreSuccessScreen(
             modifier = Modifier.padding(horizontal = 20.dp),
         )
 
-        if (productCount != 0 || searchTerm.isNullOrEmpty()) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = NapzakMarketTheme.colors.gray10)
-                    .padding(horizontal = 20.dp)
-                    .padding(top = 15.dp, bottom = 12.dp),
-                horizontalArrangement = Arrangement.spacedBy(6.dp),
-            ) {
-                GenreFilterChip(
-                    genreList = filteredGenres,
-                    onChipClick = onGenreFilterClick,
-                )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = NapzakMarketTheme.colors.gray10)
+                .padding(horizontal = 20.dp)
+                .padding(top = 15.dp, bottom = 12.dp),
+            horizontalArrangement = Arrangement.spacedBy(6.dp),
+        ) {
+            GenreFilterChip(
+                genreList = filteredGenres,
+                onChipClick = onGenreFilterClick,
+            )
 
-                if (selectedTab == TradeType.SELL) {
-                    BasicFilterChip(
-                        filterName = stringResource(explore_unopened),
-                        isClicked = isUnopenSelected,
-                        onChipClick = onUnopenFilterClick,
-                    )
-                }
-
+            if (selectedTab == TradeType.SELL) {
                 BasicFilterChip(
-                    filterName = stringResource(explore_exclude_sold_out),
-                    isClicked = isSoldOutSelected,
-                    onChipClick = onExcludeSoldOutFilterClick,
+                    filterName = stringResource(explore_unopened),
+                    isClicked = isUnopenSelected,
+                    onChipClick = onUnopenFilterClick,
                 )
             }
 
-            GenreAndProductList(
-                genreList = filteredGenres,
-                productListState = listState,
-                productCount = productCount,
-                productList = products,
-                sortType = sortOption,
-                onGenreButtonClick = onGenreDetailNavigate,
-                onSortOptionClick = { onSortOptionClick(sortOption) },
-                onProductClick = onProductDetailNavigate,
-                onLikeButtonClick = onLikeButtonClick,
+            BasicFilterChip(
+                filterName = stringResource(explore_exclude_sold_out),
+                isClicked = isSoldOutSelected,
+                onChipClick = onExcludeSoldOutFilterClick,
             )
-        } else {
-            if (searchTerm.isNotEmpty()) EmptySearchResultView()
+        }
+
+        GenreAndProductList(
+            searchTerm = searchTerm,
+            genreList = filteredGenres,
+            productListState = listState,
+            productCount = productCount,
+            productList = products,
+            sortType = sortOption,
+            onGenreButtonClick = onGenreDetailNavigate,
+            onSortOptionClick = { onSortOptionClick(sortOption) },
+            onProductClick = onProductDetailNavigate,
+            onLikeButtonClick = onLikeButtonClick,
+        )
+
+        if (!searchTerm.isNullOrEmpty() && productCount==0) {
+            EmptySearchResultView()
         }
     }
 
@@ -395,6 +394,7 @@ private fun ExploreSearchTextField(
 
 @Composable
 private fun GenreAndProductList(
+    searchTerm: String?,
     genreList: List<Genre>,
     productListState: LazyListState,
     productCount: Int,
@@ -408,7 +408,6 @@ private fun GenreAndProductList(
     LazyColumn(
         state = productListState,
         modifier = Modifier
-            .fillMaxSize()
             .background(color = NapzakMarketTheme.colors.white),
     ) {
         item {
@@ -426,92 +425,95 @@ private fun GenreAndProductList(
                         )
                     }
                 }
+            }
+        }
 
+        if (productList.isNotEmpty() || searchTerm.isNullOrEmpty()) {
+            item {
+                Column {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 20.dp, top = 20.dp, end = 20.dp),
+                    ) {
+                        Text(
+                            text = buildAnnotatedString {
+                                withStyle(style = SpanStyle(color = NapzakMarketTheme.colors.gray500)) {
+                                    append(stringResource(explore_product))
+                                }
+                                withStyle(style = SpanStyle(color = NapzakMarketTheme.colors.purple500)) {
+                                    append(stringResource(explore_count, productCount))
+                                }
+                            },
+                            style = NapzakMarketTheme.typography.body14sb,
+                        )
+
+                        Spacer(Modifier.weight(1f))
+
+                        Row(
+                            modifier = Modifier.noRippleClickable(onSortOptionClick),
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Text(
+                                text = sortType.label,
+                                style = NapzakMarketTheme.typography.caption12sb,
+                                color = NapzakMarketTheme.colors.gray200,
+                            )
+
+                            Spacer(Modifier.width(3.dp))
+
+                            Icon(
+                                imageVector = ImageVector.vectorResource(ic_gray_arrow_down),
+                                contentDescription = null,
+                                tint = NapzakMarketTheme.colors.gray200,
+                                modifier = Modifier.size(width = 7.dp, height = 4.dp),
+                            )
+                        }
+                    }
+                }
+            }
+
+            itemsIndexed(productList.chunked(2)) { index, rowItems ->
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(start = 20.dp, top = 20.dp, end = 20.dp),
+                        .padding(top = 20.dp)
+                        .padding(horizontal = 20.dp),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp),
                 ) {
-                    Text(
-                        text = buildAnnotatedString {
-                            withStyle(style = SpanStyle(color = NapzakMarketTheme.colors.gray500)) {
-                                append(stringResource(explore_product))
-                            }
-                            withStyle(style = SpanStyle(color = NapzakMarketTheme.colors.purple500)) {
-                                append(stringResource(explore_count, productCount))
-                            }
-                        },
-                        style = NapzakMarketTheme.typography.body14sb,
-                    )
+                    rowItems.forEach { product ->
+                        with(product) {
+                            NapzakLargeProductItem(
+                                genre = genreName,
+                                title = productName,
+                                imgUrl = photo,
+                                price = price.toString(),
+                                createdDate = uploadTime,
+                                reviewCount = chatCount.toString(),
+                                likeCount = interestCount.toString(),
+                                isLiked = isInterested,
+                                isMyItem = isOwnedByCurrentUser,
+                                isSellElseBuy = TradeType.valueOf(tradeType) == TradeType.SELL,
+                                isSuggestionAllowed = isPriceNegotiable,
+                                tradeStatus = TradeStatusType.get(
+                                    tradeStatus, TradeType.valueOf(tradeType)
+                                ),
+                                onLikeClick = { onLikeButtonClick(productId, isInterested) },
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .noRippleClickable { onProductClick(productId, tradeType) },
+                            )
+                        }
+                    }
 
-                    Spacer(Modifier.weight(1f))
-
-                    Row(
-                        modifier = Modifier.noRippleClickable(onSortOptionClick),
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        Text(
-                            text = sortType.label,
-                            style = NapzakMarketTheme.typography.caption12sb,
-                            color = NapzakMarketTheme.colors.gray200,
-                        )
-
-                        Spacer(Modifier.width(3.dp))
-
-                        Icon(
-                            imageVector = ImageVector.vectorResource(ic_gray_arrow_down),
-                            contentDescription = null,
-                            tint = NapzakMarketTheme.colors.gray200,
-                            modifier = Modifier.size(width = 7.dp, height = 4.dp),
-                        )
+                    if (rowItems.size == 1) {
+                        Spacer(modifier = Modifier.weight(1f))
                     }
                 }
             }
+
+            item { Spacer(Modifier.height(32.dp)) }
         }
-
-        itemsIndexed(productList.chunked(2)) { index, rowItems ->
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 20.dp)
-                    .padding(horizontal = 20.dp),
-                horizontalArrangement = Arrangement.spacedBy(20.dp),
-            ) {
-                rowItems.forEach { product ->
-                    with(product) {
-                        NapzakLargeProductItem(
-                            genre = genreName,
-                            title = productName,
-                            imgUrl = photo,
-                            price = price.toString(),
-                            createdDate = uploadTime,
-                            reviewCount = chatCount.toString(),
-                            likeCount = interestCount.toString(),
-                            isLiked = isInterested,
-                            isMyItem = isOwnedByCurrentUser,
-                            isSellElseBuy = TradeType.valueOf(tradeType) == TradeType.SELL,
-                            isSuggestionAllowed = isPriceNegotiable,
-                            tradeStatus = TradeStatusType.get(
-                                tradeStatus, TradeType.valueOf(tradeType)
-                            ),
-                            onLikeClick = { onLikeButtonClick(productId, isInterested) },
-                            modifier = Modifier
-                                .weight(1f)
-                                .noRippleClickable { onProductClick(productId, tradeType) },
-                        )
-                    }
-                }
-
-                if (rowItems.size == 1) {
-                    Spacer(modifier = Modifier.weight(1f))
-                }
-            }
-        }
-
-        item {
-            Spacer(Modifier.height(32.dp))
-        }
-
     }
 }
 
