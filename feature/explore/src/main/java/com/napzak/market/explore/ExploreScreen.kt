cf2b8ed2dac6510e7f2d0210e2fcf259c2f39c8f
chrin05 <rinring105@gmail.com>
2025-09-23 20:43:50 +0900

    [mod/#361] 탐색탭 상세페이지에서 돌아왔을 때 스크롤 위치 유지되도록 변경

--- feature/explore/src/main/java/com/napzak/market/explore/ExploreScreen.kt
+++ feature/explore/src/main/java/com/napzak/market/explore/ExploreScreen.kt
@@ -23,6 +23,9 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -253,9 +256,13 @@ private fun ExploreSuccessScreen(
         skipPartiallyExpanded = true
     )
     val listState = rememberLazyListState()
+    var lastSelectedTab by remember { mutableStateOf(selectedTab) }
 
     LaunchedEffect(selectedTab) {
-        listState.animateScrollToItem(0)
+        if (lastSelectedTab!= selectedTab) {
+            lastSelectedTab = selectedTab
+            listState.animateScrollToItem(0)
+        }
     }
 
     Column(
