acbc4ee40c5a52bb25cab580f053b0f5cd0bff2d
boiledegg <tjrwns2163@naver.com>
2026-04-26 15:25:18 +0900

    [feat/#388] 뷰모델에 NavKey를 수동 주입하는 확장함수를 구현

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -2,6 +2,7 @@ import com.napzak.market.buildlogic.dsl.setNameSpace
 
 plugins {
     id("com.napzak.market.buildlogic.convention.feature")
+    id("com.napzak.market.buildlogic.convention.compose")
 }
 
 android {
@@ -12,6 +13,7 @@ dependencies {
     api(libs.androidx.navigation3.runtime)
     api(libs.androidx.navigation3.ui)
     api(libs.androidx.lifecycle.viewmodel.navigation3)
+    implementation(libs.hilt.navigation.compose)
 
     implementation(projects.core.common)
 }
