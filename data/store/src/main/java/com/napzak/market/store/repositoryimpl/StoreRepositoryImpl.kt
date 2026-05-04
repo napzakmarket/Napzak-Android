08d7077eba88724b81de11138678ed0a90841ec1
김채린 <chrin105@naver.com>
2026-05-04 16:38:50 +0900

    [feat/#386] repository, repositoryImpl 구현

--- data/store/src/main/java/com/napzak/market/store/repositoryimpl/StoreRepositoryImpl.kt
+++ data/store/src/main/java/com/napzak/market/store/repositoryimpl/StoreRepositoryImpl.kt
@@ -1,12 +1,15 @@
 package com.napzak.market.store.repositoryimpl
 
 import com.napzak.market.store.datasource.StoreDataSource
+import com.napzak.market.store.dto.request.CodeVerificationRequest
 import com.napzak.market.store.dto.request.GenreRegistrationRequest
 import com.napzak.market.store.dto.request.NicknameRequest
+import com.napzak.market.store.dto.request.PhoneCodeRequest
 import com.napzak.market.store.dto.request.WithdrawRequest
 import com.napzak.market.store.mapper.toDomain
 import com.napzak.market.store.mapper.toRequest
 import com.napzak.market.store.model.Genre
+import com.napzak.market.store.model.PhoneCodeVerificationResult
 import com.napzak.market.store.model.StoreDetail
 import com.napzak.market.store.model.StoreEditProfile
 import com.napzak.market.store.model.StoreInfo
@@ -30,7 +33,8 @@ class StoreRepositoryImpl @Inject constructor(
     }
 
     override suspend fun postRegisterGenres(genreIds: List<Long>): Result<Genre> = runCatching {
-        storeDataSource.postRegistrationGenre(GenreRegistrationRequest(genreIds)).data.genreList.first().toDomain()
+        storeDataSource.postRegistrationGenre(GenreRegistrationRequest(genreIds)).data.genreList.first()
+            .toDomain()
     }
 
     override suspend fun logout(): Result<Unit> = runCatching {
@@ -41,7 +45,7 @@ class StoreRepositoryImpl @Inject constructor(
     override suspend fun withdraw(title: String, description: String?): Result<UserWithdrawal> =
         runCatching {
             storeDataSource.withdraw(WithdrawRequest(title, description)).data.toDomain()
-    }
+        }
 
     override suspend fun fetchStoreInfo(): Result<StoreInfo> = runCatching {
         storeDataSource.getStoreInfo().toDomain()
@@ -55,15 +59,16 @@ class StoreRepositoryImpl @Inject constructor(
         storeDataSource.getStoreDetail(storeId).toDomain()
     }
 
-    override suspend fun updateEditProfile(request: StoreEditProfile): Result<StoreEditProfile> = runCatching {
-        storeDataSource.updateEditProfile(request.toRequest()).toDomain()
-    }
+    override suspend fun updateEditProfile(request: StoreEditProfile): Result<StoreEditProfile> =
+        runCatching {
+            storeDataSource.updateEditProfile(request.toRequest()).toDomain()
+        }
 
     override suspend fun getTermsAgreement(): Result<TermsAgreement> = runCatching {
         storeDataSource.getTermsAgreement().toDomain()
     }
 
-    override suspend fun postTermsAgreement(bundleId: Int) : Result<Unit> = runCatching {
+    override suspend fun postTermsAgreement(bundleId: Int): Result<Unit> = runCatching {
         storeDataSource.postTermsAgreement(bundleId)
     }
 
@@ -73,5 +78,24 @@ class StoreRepositoryImpl @Inject constructor(
 
     override suspend fun unblockStore(storeId: Long): Result<Unit> = runCatching {
         storeDataSource.unblockStore(storeId)
+    }
+
+    override suspend fun getPhoneVerificationStatus(): Result<Boolean> = runCatching {
+        storeDataSource.getPhoneVerificationStatus().toDomain()
+    }
+
+    override suspend fun postPhoneVerificationCode(phoneNumber: String): Result<Int> = runCatching {
+        storeDataSource.postPhoneVerificationCode(
+            request = PhoneCodeRequest(phoneNumber)
+        ).toDomain()
+    }
+
+    override suspend fun checkPhoneVerificationCode(
+        phoneNumber: String,
+        code: String
+    ): Result<PhoneCodeVerificationResult> = runCatching {
+        storeDataSource.checkPhoneVerificationCode(
+            request = CodeVerificationRequest(phoneNumber, code)
+        ).toDomain()
     }
 }
