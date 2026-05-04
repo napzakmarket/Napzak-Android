b2a55b1f2b9158eb0c6e23f9b78a47e1b0d62a21
김채린 <chrin105@naver.com>
2026-05-04 16:38:50 +0900

    [feat/#386] repository, repositoryImpl 구현

--- data/store/src/main/java/com/napzak/market/store/mapper/StoreMapper.kt
+++ data/store/src/main/java/com/napzak/market/store/mapper/StoreMapper.kt
@@ -1,17 +1,21 @@
 package com.napzak.market.store.mapper
 
+import com.napzak.market.store.dto.response.CodeVerificationResponse
 import com.napzak.market.store.dto.response.GenreDto
 import com.napzak.market.store.dto.response.KakaoLoginResponse
+import com.napzak.market.store.dto.response.PhoneCodeResponse
+import com.napzak.market.store.dto.response.PhoneVerificationStatusResponse
+import com.napzak.market.store.dto.response.StoreResponse
+import com.napzak.market.store.dto.response.TermsResponse
 import com.napzak.market.store.dto.response.WithdrawResponse
 import com.napzak.market.store.model.Genre
-import com.napzak.market.store.model.UserWithdrawal
-import com.napzak.market.store.dto.response.StoreResponse
 import com.napzak.market.store.model.KakaoLogin
-import com.napzak.market.store.dto.response.TermsResponse
+import com.napzak.market.store.model.PhoneCodeVerificationResult
 import com.napzak.market.store.model.StoreInfo
-import com.napzak.market.store.model.UserRole
 import com.napzak.market.store.model.Terms
 import com.napzak.market.store.model.TermsAgreement
+import com.napzak.market.store.model.UserRole
+import com.napzak.market.store.model.UserWithdrawal
 
 fun GenreDto.toDomain(): Genre {
     return Genre(
@@ -55,5 +59,20 @@ fun KakaoLoginResponse.toDomain(): KakaoLogin {
         refreshToken = refreshToken,
         nickname = nickname,
         role = UserRole.from(role).toString()
+    )
+}
+
+fun PhoneVerificationStatusResponse.toDomain(): Boolean {
+    return this.isPhoneVerified
+}
+
+fun PhoneCodeResponse.toDomain(): Int {
+    return this.remainingRequestCount
+}
+
+fun CodeVerificationResponse.toDomain(): PhoneCodeVerificationResult {
+    return PhoneCodeVerificationResult(
+        isPhoneVerified = isPhoneVerified,
+        remainingRequestCount = remainingRequestCount,
     )
 }
