a266c3e4cbbea0655ede8f6489cf513d80a9f194
boiledegg <tjrwns2163@naver.com>
2026-05-05 15:42:56 +0900

    [feat/#392] FeatureDataStore에 접근하기 위한 저장소 클래스 추가

--- domain/product/src/main/java/com/napzak/market/product/repository/ProductDetailRepository.kt
+++ domain/product/src/main/java/com/napzak/market/product/repository/ProductDetailRepository.kt
@@ -1,9 +1,12 @@
 package com.napzak.market.product.repository
 
 import com.napzak.market.product.model.ProductDetail
+import kotlinx.coroutines.flow.Flow
 
 interface ProductDetailRepository {
     suspend fun getProductDetail(productId: Long): Result<ProductDetail>
     suspend fun patchTradeStatus(productId: Long, tradeStatus: String): Result<Unit>
     suspend fun deleteProduct(productId: Long): Result<Unit>
+    fun getShowProductStatusTooltipFlow(): Flow<Boolean>
+    suspend fun setShowProductStatusTooltip(value: Boolean)
 }
