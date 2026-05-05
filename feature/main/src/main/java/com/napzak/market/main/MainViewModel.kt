4ccf13ec1aac5ed9a9a236d3eff7f0cdc55dddde
Chaerin Kim <rinring105@gmail.com>
2026-05-05 21:16:57 +0900

    [feat/#386] mainViewModel 추가

---
+++ feature/main/src/main/java/com/napzak/market/main/MainViewModel.kt
@@ -0,0 +1,25 @@
+package com.napzak.market.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.napzak.market.store.usecase.GetPhoneVerificationStatusUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val getPhoneVerificationStatus: GetPhoneVerificationStatusUseCase,
+) : ViewModel() {
+    private val _isPhoneVerified = MutableStateFlow(false)
+    val isPhoneVerified = _isPhoneVerified.asStateFlow()
+
+    fun checkPhoneVerification() = viewModelScope.launch {
+        getPhoneVerificationStatus()
+            .onSuccess { isVerified ->
+                _isPhoneVerified.value = isVerified
+            }
+    }
+}
